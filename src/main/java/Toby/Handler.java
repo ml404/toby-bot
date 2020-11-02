63c8c780c96520714f2e53b635d096321427e004
Matthew Layton <ml404@kent.ac.uk>
2020-11-02 15:10:11 +0000

    dice roll NPE fix

--- src/main/java/Toby/Handler.java
+++ src/main/java/Toby/Handler.java
@@ -95,7 +95,8 @@ public class Handler extends ListenerAdapter {
                 // have object returns and will instead have Void returns. You can still use the flatMap operator to run chain another RestAction!
 
                 Random rand = ThreadLocalRandom.current();
-                int diceRoll = Optional.of(Integer.parseInt(msg.split(" ")[1])).orElse(6);
+                Optional<String> s = Optional.ofNullable(msg.split(" ")[1]);
+                int diceRoll = s.map(Integer::parseInt).orElse(6);
                 int roll = rand.nextInt(diceRoll) + 1; //This results in 1 - 6 (instead of 0 - 5) for default value
                 channel.sendMessage("Your roll: " + roll)
                         .flatMap(
