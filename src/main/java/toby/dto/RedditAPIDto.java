9b54988bccd02290db8fa41a983d9ea0cc41fdfc
Matthew Layton <15813613+ml404@users.noreply.github.com>
2020-11-06 18:19:28 +0000

    Fix filter for week

--- src/main/java/toby/dto/RedditAPIDto.java
+++ src/main/java/toby/dto/RedditAPIDto.java
@@ -23,7 +23,7 @@ public class RedditAPIDto {
     private Boolean video;
 
 
-    public static String redditPrefix ="https://old.reddit.com/r/%s/top/.json?limit=15";
+    public static String redditPrefix ="https://old.reddit.com/r/%s/top/.json?limit=15&t=week";
     public static String commentsPrefix ="https://old.reddit.com%s";
 
     public String getTitle() {
