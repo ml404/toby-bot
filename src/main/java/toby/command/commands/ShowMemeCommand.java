016d717ed4841d191450816911ba6aa053c82755
Matthew Layton <15813613+ml404@users.noreply.github.com>
2020-11-06 16:16:25 +0000

    showmeme command

---
+++ src/main/java/toby/command/commands/ShowMemeCommand.java
@@ -0,0 +1,65 @@
+package toby.command.commands;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.gson.Gson;
+import me.duncte123.botcommons.messaging.EmbedUtils;
+import me.duncte123.botcommons.web.WebUtils;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
+import toby.command.CommandContext;
+import toby.command.ICommand;
+import toby.dto.RedditAPIDto;
+
+import java.util.List;
+import java.util.Random;
+
+public class ShowMemeCommand implements ICommand {
+    @Override
+    public void handle(CommandContext ctx) {
+        Gson gson = new Gson();
+        final TextChannel channel = ctx.getChannel();
+        List<String> args = ctx.getArgs();
+        if (args.size() == 0) {
+            getHelp();
+        } else {
+            String subredditArg = args.get(0);
+            WebUtils.ins.getJSONObject(String.format(RedditAPIDto.redditPrefix, subredditArg)).async((json) -> {
+                if ((json.get("data").get("dist").asInt() == 0)) {
+                    channel.sendMessage(String.format("I think you typo'd the subreddit: %s", subredditArg)).queue();
+                    System.out.println(json);
+                    return;
+                }
+
+                final JsonNode parentData = json.get("data");
+                final JsonNode children = parentData.get("children");
+                Random random = new Random();
+                JsonNode meme = children.get(random.nextInt(children.size()));
+                RedditAPIDto redditAPIDto = gson.fromJson(meme.get("data").toString(), RedditAPIDto.class);
+                if (redditAPIDto.isNsfw()) {
+                    channel.sendMessage(String.format("I received a NSFW subreddit by %s, or reddit gave me a NSFW meme, either way somebody shoot that guy", ctx.getAuthor())).queue();
+                } else if (redditAPIDto.getVideo()) {
+                    channel.sendMessage("I pulled back a video, whoops. Try again maybe?").queue();
+                } else {
+                    String title = redditAPIDto.getTitle();
+                    String url = redditAPIDto.getUrl();
+                    String image = redditAPIDto.getImage();
+                    EmbedBuilder embed = EmbedUtils.embedImageWithTitle(title, url, image);
+                    channel.sendMessage(embed.build()).queue();
+
+                }
+            });
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "showmeme";
+    }
+
+    @Override
+    public String getHelp() {
+        return "This command shows a meme from the subreddit you've specified (SFW only) \n" +
+                "Usage: `!showmeme raimimemes` \n";
+    }
+}
+
---
+++ src/main/java/toby/dto/RedditAPIDto.java
@@ -0,0 +1,75 @@
+package toby.dto;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RedditAPIDto {
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("author")
+    private String author;
+
+    @SerializedName("url")
+    private String url;
+
+    @SerializedName("url_overridden_by_dest")
+    private String image;
+
+    @SerializedName("over_18")
+    private Boolean nsfw;
+
+    @SerializedName("is_video")
+    private Boolean video;
+
+
+    public static String redditPrefix ="https://old.reddit.com/r/%s/hot/.json";
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Boolean isNsfw() {
+        return nsfw;
+    }
+
+    public void setNsfw(Boolean nsfw) {
+        this.nsfw = nsfw;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Boolean getVideo() {
+        return video;
+    }
+
+    public void setVideo(Boolean video) {
+        this.video = video;
+    }
+}
