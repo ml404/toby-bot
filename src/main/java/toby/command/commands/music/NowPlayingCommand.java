61bd81cefe6847570b7c3c513a7026956f244644
Matthew Layton <15813613+ml404@users.noreply.github.com>
2021-03-16 11:50:32 +0000

    timestamp fix

--- src/main/java/toby/command/commands/music/NowPlayingCommand.java
+++ src/main/java/toby/command/commands/music/NowPlayingCommand.java
@@ -55,7 +55,7 @@ public class NowPlayingCommand implements ICommand {
         String songDuration = QueueCommand.formatTime(duration);
 
         String nowPlaying = String.format("Now playing `%s` by `%s` (Link: <%s>) \n", info.title, info.author, info.uri);
-        String timeStamp = String.format("`%s`\\`%s`", songPosition, songDuration);
+        String timeStamp = String.format("`[%s/%s]`", songPosition, songDuration);
         channel.sendMessage(nowPlaying + timeStamp).queue();
     }
 
