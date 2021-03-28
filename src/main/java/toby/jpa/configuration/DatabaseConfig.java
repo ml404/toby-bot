000a1cbdf09c101347e7f7e6c48bf78d562c9ae8
Matthew Layton <15813613+ml404@users.noreply.github.com>
2021-03-28 19:02:19 +0100

    config tweak for db

--- src/main/java/toby/jpa/configuration/DatabaseConfig.java
+++ src/main/java/toby/jpa/configuration/DatabaseConfig.java
@@ -1,13 +1,12 @@
 package toby.jpa.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -15,9 +14,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
 
-@Configuration
+@Component
 @EnableTransactionManagement
-@ComponentScan
 public class DatabaseConfig {
 
     @Bean
