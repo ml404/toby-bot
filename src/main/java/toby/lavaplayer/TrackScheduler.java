5cd4e13a9aa2750b480341e7aff98ce27579f88b
Matthew Layton <15813613+ml404@users.noreply.github.com>
2021-04-05 14:51:47 +0100

    add some info to ontrackend

--- src/main/java/toby/lavaplayer/TrackScheduler.java
+++ src/main/java/toby/lavaplayer/TrackScheduler.java
@@ -4,6 +4,9 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+import net.dv8tion.jda.api.entities.TextChannel;
+import toby.command.commands.music.QueueCommand;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -12,6 +15,9 @@ public class TrackScheduler extends AudioEventAdapter {
     private final AudioPlayer player;
     private BlockingQueue<AudioTrack> queue;
     private boolean isLooping;
+    private TextChannel currentTextChannel;
+
+
     public boolean isLooping() {
         return isLooping;
     }
@@ -25,7 +31,8 @@ public class TrackScheduler extends AudioEventAdapter {
         this.queue = new LinkedBlockingQueue<>();
     }
 
-    public void queue(AudioTrack track) {
+    public void queue(AudioTrack track, TextChannel channel) {
+        this.currentTextChannel = channel;
         if (!this.player.startTrack(track, true)) {
             this.queue.offer(track);
         }
@@ -45,6 +52,7 @@ public class TrackScheduler extends AudioEventAdapter {
             }
             PlayerManager.getInstance().setCurrentlyStoppable(true);
             nextTrack();
+            nowPlaying(currentTextChannel, track);
         }
     }
 
@@ -67,5 +75,14 @@ public class TrackScheduler extends AudioEventAdapter {
 
     public void setQueue(BlockingQueue<AudioTrack> queue) {
         this.queue = queue;
+    }
+
+
+    private void nowPlaying(TextChannel channel, AudioTrack track) {
+        AudioTrackInfo info = track.getInfo();
+        long duration = track.getDuration();
+        String songDuration = QueueCommand.formatTime(duration);
+        String nowPlaying = String.format("Now playing `%s` by `%s` `[%s]` (Link: <%s>) ", info.title, info.author, songDuration, info.uri);
+        channel.sendMessage(nowPlaying).queue();
     }
 }
