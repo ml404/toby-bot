aebfb0f975178a55bd04f108c186b448da6f3ddc
Matthew Layton <15813613+ml404@users.noreply.github.com>
2021-06-07 17:13:04 +0100

    DbdRandomKillerCommand + extra testing for wiki fetching commands

--- src/main/java/toby/command/commands/fetch/DbdRandomKillerCommand.java
+++ src/main/java/toby/command/commands/fetch/DbdRandomKillerCommand.java
@@ -12,14 +12,15 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-public class Kf2RandomMapCommand implements IFetchCommand {
+public class DbdRandomKillerCommand implements IFetchCommand {
 
-    private static final String kf2WebUrl = "https://wiki.killingfloor2.com/index.php?title=Maps_(Killing_Floor_2)";
-    private static final String cacheName = "kf2Maps";
-    private static final String className = "mw-parser-output";
+    private static final String dbdWebUrl = "https://deadbydaylight.fandom.com/wiki/Killers";
+    public static final String cacheName = "dbdKillers";
+    public static final String className = "mw-content-ltr";
+    public static final String cssQuery = "div";
     private final Cache cache;
 
-    public Kf2RandomMapCommand(Cache loadingCache) {
+    public DbdRandomKillerCommand(Cache loadingCache) {
         this.cache = loadingCache;
     }
 
@@ -30,8 +31,9 @@ public class Kf2RandomMapCommand implements IFetchCommand {
         final TextChannel channel = ctx.getChannel();
         try {
             WikiFetcher wikiFetcher = new WikiFetcher(cache);
-            List<String> kf2Maps = wikiFetcher.fetchFromWiki(cacheName, kf2WebUrl, className, "b");
-            channel.sendMessage(RandomCommand.getRandomElement(kf2Maps)).queue(message -> ICommand.deleteAfter(message, deleteDelay));
+            List<String> dbdKillers = wikiFetcher.fetchFromWiki(cacheName, dbdWebUrl, className, cssQuery);
+            channel.sendMessage(RandomCommand.getRandomElement(dbdKillers)).queue(message -> ICommand.deleteAfter(message, deleteDelay));
+
         } catch (IOException ignored) {
             channel.sendMessage("Huh, the website I pull data from must have returned something unexpected.").queue(message -> ICommand.deleteAfter(message, deleteDelay));
         }
@@ -39,18 +41,18 @@ public class Kf2RandomMapCommand implements IFetchCommand {
 
     @Override
     public String getName() {
-        return "kf2";
+        return "dbd";
     }
 
     @Override
     public String getHelp(String prefix) {
-        return "return a random kf2 map \n" +
-                String.format("Usage: %skf2", prefix) +
+        return "return a random dead by daylight killer \n" +
+                String.format("Usage: %sdbd", prefix) +
                 String.format("Aliases are: '%s'", String.join(",", getAliases()));
     }
 
     @Override
     public List<String> getAliases() {
-        return Arrays.asList("kf2map", "kfmap", "kfrand");
+        return Arrays.asList("dbd", "killer");
     }
 }
