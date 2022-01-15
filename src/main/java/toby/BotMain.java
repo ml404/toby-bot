997c9b4bd52f2cff4714002c6ad574dfd960a17e
Matthew Layton <15813613+ml404@users.noreply.github.com>
2022-01-15 14:42:57 +0000

    social credit leaderboard excuse fix for over 2000 characters update a lot of old dependencies

--- src/main/java/toby/BotMain.java
+++ src/main/java/toby/BotMain.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -37,15 +38,18 @@ public class BotMain {
 
         String token = configService.getConfigByName("TOKEN", "all").getValue();
         JDABuilder builder = JDABuilder.createDefault(token,
-                GatewayIntent.GUILD_MEMBERS,
-                GatewayIntent.GUILD_MESSAGES,
-                GatewayIntent.GUILD_MESSAGE_REACTIONS,
-                GatewayIntent.GUILD_VOICE_STATES,
-                GatewayIntent.GUILD_EMOJIS
-        ).disableCache(EnumSet.of(
-                CacheFlag.CLIENT_STATUS,
-                CacheFlag.ACTIVITY
-        )).enableCache(CacheFlag.VOICE_STATE, CacheFlag.EMOTE);
+                        GatewayIntent.GUILD_MEMBERS,
+                        GatewayIntent.GUILD_MESSAGES,
+                        GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                        GatewayIntent.GUILD_VOICE_STATES,
+                        GatewayIntent.GUILD_EMOJIS)
+                //TODO: circle back to see if I can do something different here,
+                // without this get members only grabs those who are currently connected to a voice channel at time of request
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
+                .disableCache(EnumSet.of(
+                        CacheFlag.CLIENT_STATUS,
+                        CacheFlag.ACTIVITY
+                )).enableCache(CacheFlag.VOICE_STATE, CacheFlag.EMOTE);
         builder.addEventListeners(new Handler(configService, brotherService, userService, musicFileService, excuseService, waiter), waiter);
         setJda(builder.build());
     }
