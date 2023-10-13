debbbbde0fe49fbb4cfb48c9b984cae8166874d5
Matthew Layton <15813613+ml404@users.noreply.github.com>
2023-10-13 10:26:09 +0100

    adjust controller test

--- src/test/java/controller/BotControllerIT.java
+++ src/test/java/controller/BotControllerIT.java
@@ -4,10 +4,12 @@ package controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 import toby.Application;
 
 import java.net.URL;
@@ -15,6 +17,7 @@ import java.net.URL;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
+@AutoConfigureMockMvc
 public class BotControllerIT {
 
     @LocalServerPort
@@ -24,6 +27,9 @@ public class BotControllerIT {
 
     @Autowired
     private TestRestTemplate template;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @BeforeEach
     public void setUp() throws Exception {
