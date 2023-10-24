a2245ad3671e31b6348845c912ce624c3f96171e
Matthew Layton <15813613+ml404@users.noreply.github.com>
2023-10-24 18:03:01 +0100

    more attachment test cases

--- src/test/java/toby/command/commands/music/IntroSongCommandTest.java
+++ src/test/java/toby/command/commands/music/IntroSongCommandTest.java
@@ -1,18 +1,25 @@
 package toby.command.commands.music;
 
 import net.dv8tion.jda.api.entities.Mentions;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.utils.AttachmentProxy;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import toby.command.CommandContext;
 import toby.jpa.dto.ConfigDto;
+import toby.jpa.dto.MusicDto;
 import toby.jpa.service.IConfigService;
 import toby.jpa.service.IMusicFileService;
 import toby.jpa.service.IUserService;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import static org.mockito.Mockito.*;
 
@@ -39,6 +46,9 @@ class IntroSongCommandTest implements MusicCommandTest {
     @AfterEach
     void tearDown() {
         tearDownCommonMusicMocks();
+        reset(userService);
+        reset(musicFileService);
+        reset(configService);
     }
 
     @Test
@@ -58,8 +68,9 @@ class IntroSongCommandTest implements MusicCommandTest {
         introSongCommand.handleMusicCommand(commandContext, playerManager, requestingUserDto, 0);
 
         //Assert
-        verify(interactionHook, timeout(1)).deleteOriginal();
-        verify(interactionHook, timeout(1)).sendMessageFormat(eq("Successfully set %s's intro song to '%s' with volume '%d'"), eq("UserName"), eq("https://www.youtube.com/"), eq(20));
+        verify(interactionHook, times(1)).deleteOriginal();
+        verify(musicFileService, times(1)).createNewMusicFile(any(MusicDto.class));
+        verify(interactionHook, times(1)).sendMessageFormat(eq("Successfully set %s's intro song to '%s' with volume '%d'"), eq("UserName"), eq("https://www.youtube.com/"), eq(20));
 
     }
 
@@ -86,8 +97,79 @@ class IntroSongCommandTest implements MusicCommandTest {
         introSongCommand.handleMusicCommand(commandContext, playerManager, requestingUserDto, 0);
 
         //Assert
-        verify(interactionHook, timeout(1)).deleteOriginal();
-        verify(interactionHook, timeout(1)).sendMessageFormat(eq("You do not have adequate permissions to use this command, if you believe this is a mistake talk to the server owner: Effective Name"));
+        verify(interactionHook, times(1)).deleteOriginal();
+        verify(interactionHook, times(1)).sendMessageFormat(eq("You do not have adequate permissions to use this command, if you believe this is a mistake talk to the server owner: Effective Name"));
+
+    }
+
+    @Test
+    void testIntroSong_withSuperuser_andValidAttachement_setsIntroViaAttachment_andCreatesNewMusicFile() throws ExecutionException, InterruptedException, IOException {
+        //Arrange
+        CommandContext commandContext = new CommandContext(event);
+        OptionMapping attachmentOptionMapping = mock(OptionMapping.class);
+        OptionMapping userOptionMapping = mock(OptionMapping.class);
+
+        when(event.getOption("users")).thenReturn(userOptionMapping);
+        when(event.getOption("attachment")).thenReturn(attachmentOptionMapping);
+        Message.Attachment messageAttachment = mock(Message.Attachment.class);
+        AttachmentProxy attachmentProxy = mock(AttachmentProxy.class);
+        InputStream inputStream = mock(InputStream.class);
+        when(attachmentOptionMapping.getAsAttachment()).thenReturn(messageAttachment);
+        when(messageAttachment.getFileExtension()).thenReturn("mp3");
+        when(messageAttachment.getSize()).thenReturn(1000);
+        when(messageAttachment.getProxy()).thenReturn(attachmentProxy);
+        when(messageAttachment.getFileName()).thenReturn("filename");
+        CompletableFuture completableFuture = mock(CompletableFuture.class);
+        when(attachmentProxy.download()).thenReturn(completableFuture);
+        when(completableFuture.get()).thenReturn(inputStream);
+        when(inputStream.readAllBytes()).thenReturn(new byte[0]);
+        requestingUserDto.setMusicDto(null);
+        when(userService.listGuildUsers(1L)).thenReturn(List.of(requestingUserDto));
+        when(configService.getConfigByName("DEFAULT_VOLUME", "1")).thenReturn(new ConfigDto("DEFAULT_VOLUME", "20", "1"));
+
+        //Act
+        introSongCommand.handleMusicCommand(commandContext, playerManager, requestingUserDto, 0);
+
+        //Assert
+        verify(interactionHook, times(1)).deleteOriginal();
+        verify(musicFileService, times(1)).createNewMusicFile(any(MusicDto.class));
+        verify(userService, times(1)).updateUser(eq(requestingUserDto));
+        verify(interactionHook, times(1)).sendMessageFormat(eq("Successfully set %s's intro song to '%s' with volume '%d'"), eq("UserName"), eq("filename"), eq(20));
+
+    }
+
+    @Test
+    void testIntroSong_withSuperuser_andValidAttachement_setsIntroViaAttachment_andUpdatesMusicFile() throws ExecutionException, InterruptedException, IOException {
+        //Arrange
+        CommandContext commandContext = new CommandContext(event);
+        OptionMapping attachmentOptionMapping = mock(OptionMapping.class);
+        OptionMapping userOptionMapping = mock(OptionMapping.class);
+
+        when(event.getOption("users")).thenReturn(userOptionMapping);
+        when(event.getOption("attachment")).thenReturn(attachmentOptionMapping);
+        Message.Attachment messageAttachment = mock(Message.Attachment.class);
+        AttachmentProxy attachmentProxy = mock(AttachmentProxy.class);
+        InputStream inputStream = mock(InputStream.class);
+        when(attachmentOptionMapping.getAsAttachment()).thenReturn(messageAttachment);
+        when(messageAttachment.getFileExtension()).thenReturn("mp3");
+        when(messageAttachment.getSize()).thenReturn(1000);
+        when(messageAttachment.getProxy()).thenReturn(attachmentProxy);
+        when(messageAttachment.getFileName()).thenReturn("filename");
+        CompletableFuture completableFuture = mock(CompletableFuture.class);
+        when(attachmentProxy.download()).thenReturn(completableFuture);
+        when(completableFuture.get()).thenReturn(inputStream);
+        when(inputStream.readAllBytes()).thenReturn(new byte[0]);
+        requestingUserDto.setMusicDto(new MusicDto(1L, 1L, "filename", 20, null));
+        when(userService.listGuildUsers(1L)).thenReturn(List.of(requestingUserDto));
+        when(configService.getConfigByName("DEFAULT_VOLUME", "1")).thenReturn(new ConfigDto("DEFAULT_VOLUME", "20", "1"));
+
+        //Act
+        introSongCommand.handleMusicCommand(commandContext, playerManager, requestingUserDto, 0);
+
+        //Assert
+        verify(interactionHook, times(1)).deleteOriginal();
+        verify(musicFileService, times(1)).updateMusicFile(any(MusicDto.class));
+        verify(interactionHook, times(1)).sendMessageFormat(eq("Successfully updated %s's intro song to '%s' with volume '%d'"), eq("UserName"), eq("filename"), eq(20));
 
     }
 
