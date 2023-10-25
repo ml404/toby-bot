443c29e128053e0f6604947cbf7940e444eb3938
Matthew Layton <15813613+ml404@users.noreply.github.com>
2023-10-25 12:55:42 +0100

    SetConfigCommandTest

--- src/main/java/toby/command/commands/moderation/SetConfigCommand.java
+++ src/main/java/toby/command/commands/moderation/SetConfigCommand.java
@@ -1,6 +1,7 @@
 package toby.command.commands.moderation;
 
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -10,7 +11,6 @@ import toby.jpa.dto.ConfigDto;
 import toby.jpa.dto.UserDto;
 import toby.jpa.service.IConfigService;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -22,8 +22,6 @@ import static toby.jpa.dto.ConfigDto.Configurations.*;
 public class SetConfigCommand implements IModerationCommand {
 
     private final IConfigService configService;
-    private final String CONFIG_NAME = "name";
-    private final String CONFIG_VALUE = "value";
 
     public SetConfigCommand(IConfigService configService) {
         this.configService = configService;
@@ -34,16 +32,9 @@ public class SetConfigCommand implements IModerationCommand {
         deleteAfter(ctx.getEvent().getHook(), deleteDelay);
         SlashCommandInteractionEvent event = ctx.getEvent();
         event.deferReply().queue();
-        List<OptionMapping> args = event.getOptions();
         final Member member = ctx.getMember();
-
         if (!member.isOwner()) {
             event.getHook().sendMessage("This is currently reserved for the owner of the server only, this may change in future").setEphemeral(true).queue(getConsumer(deleteDelay));
-            return;
-        }
-
-        if (args.isEmpty()) {
-            event.getHook().sendMessage(getDescription()).queue(getConsumer(deleteDelay));
             return;
         }
         validateArgumentsAndUpdateConfigs(event, deleteDelay);
@@ -51,103 +42,57 @@ public class SetConfigCommand implements IModerationCommand {
 
 
     private void validateArgumentsAndUpdateConfigs(SlashCommandInteractionEvent event, Integer deleteDelay) {
-        Optional<String> configNameStringOptional = Optional.ofNullable(event.getOption(CONFIG_NAME)).map(optionMapping -> optionMapping.getAsString().toUpperCase());
-
-        if (configNameStringOptional.isEmpty()) {
+        List<OptionMapping> options = event.getOptions();
+        if (options.isEmpty()) {
             event.getHook().sendMessage(getDescription()).queue(getConsumer(deleteDelay));
             return;
         }
-
-        ConfigDto.Configurations configName = valueOf(configNameStringOptional.get());
-
-        switch (configName) {
-            case PREFIX -> setPrefix(event, deleteDelay);
-            case MOVE -> setMove(event, deleteDelay);
-            case VOLUME -> setVolume(event, deleteDelay);
-            case DELETE_DELAY -> setDeleteDelay(event, deleteDelay);
-            default -> {
+        options.forEach(optionMapping -> {
+            switch (ConfigDto.Configurations.valueOf(optionMapping.getName())) {
+                case MOVE -> setMove(event, deleteDelay);
+                case VOLUME -> setConfigAndSendMessage(event, deleteDelay, VOLUME.name(), "Set default volume to '%s'");
+                case DELETE_DELAY -> setConfigAndSendMessage(event, deleteDelay, DELETE_DELAY.name(), "Set default delete message delay for TobyBot music messages to '%d' seconds");
+                default -> {
+                }
             }
-        }
+        });
     }
-
-    private void setDeleteDelay(SlashCommandInteractionEvent event, Integer deleteDelay) {
-        String newDefaultDelay = Optional.ofNullable(event.getOption(CONFIG_VALUE)).map(OptionMapping::getAsString).orElse("");
-        if (!newDefaultDelay.matches("\\d+")) {
-            event.getHook().sendMessage("Value given for default delete message delay for TobyBot music messages was not valid (a whole number representing seconds)").setEphemeral(true).queue(getConsumer(deleteDelay));
+    private void setConfigAndSendMessage(SlashCommandInteractionEvent event, Integer deleteDelay, String propertyName, String messageToSend) {
+        Optional<Integer> newValueOptional = Optional.ofNullable(event.getOption(propertyName)).map(OptionMapping::getAsInt);
+        if (newValueOptional.isEmpty() || newValueOptional.get() < 0) {
+            event.getHook().sendMessage("Value given valid (a whole number representing percent)").setEphemeral(true).queue(getConsumer(deleteDelay));
             return;
         }
-        String deletePropertyName = DELETE_DELAY.getConfigValue();
-        ConfigDto databaseConfig = configService.getConfigByName(deletePropertyName, event.getGuild().getId());
-        ConfigDto newConfigDto = new ConfigDto(deletePropertyName, newDefaultDelay, event.getGuild().getId());
+        Integer newDefaultVolume = newValueOptional.get();
+        ConfigDto databaseConfig = configService.getConfigByName(propertyName, event.getGuild().getId());
+        ConfigDto newConfigDto = new ConfigDto(propertyName, newDefaultVolume.toString(), event.getGuild().getId());
         if (databaseConfig != null && Objects.equals(databaseConfig.getGuildId(), newConfigDto.getGuildId())) {
             configService.updateConfig(newConfigDto);
         } else {
             configService.createNewConfig(newConfigDto);
         }
-        event.getHook().sendMessageFormat("Set default delete message delay for TobyBot music messages to '%s' seconds", newDefaultDelay).queue(getConsumer(deleteDelay));
-    }
-
-    private void setVolume(SlashCommandInteractionEvent event, Integer deleteDelay) {
-        String newDefaultVolume = Optional.ofNullable(event.getOption(CONFIG_VALUE)).map(OptionMapping::getAsString).orElse("");
-        if (!newDefaultVolume.matches("\\d+")) {
-            event.getHook().sendMessage("Value given for default volume of TobyBot music was not valid (a whole number representing percent)").setEphemeral(true).queue(getConsumer(deleteDelay));
-            return;
-        }
-        String volumePropertyName = VOLUME.getConfigValue();
-        ConfigDto databaseConfig = configService.getConfigByName(volumePropertyName, event.getGuild().getId());
-        ConfigDto newConfigDto = new ConfigDto(volumePropertyName, newDefaultVolume, event.getGuild().getId());
-        if (databaseConfig != null && Objects.equals(databaseConfig.getGuildId(), newConfigDto.getGuildId())) {
-            configService.updateConfig(newConfigDto);
-        } else {
-            configService.createNewConfig(newConfigDto);
-        }
-        event.getHook().sendMessageFormat("Set default volume to '%s'", newDefaultVolume).queue(getConsumer(deleteDelay));
+        event.getHook().sendMessageFormat(messageToSend, newDefaultVolume).queue(getConsumer(deleteDelay));
     }
 
     private void setMove(SlashCommandInteractionEvent event, Integer deleteDelay) {
-        Optional<String> newDefaultMoveChannel = Optional.ofNullable(event.getOption(CONFIG_VALUE)).map(OptionMapping::getAsString);
-        if (newDefaultMoveChannel.isPresent()) {
-            boolean newDefaultVoiceChannelExists = !event.getGuild().getVoiceChannelsByName(newDefaultMoveChannel.get(), true).isEmpty();
-            if (newDefaultVoiceChannelExists) {
-                String movePropertyName = MOVE.getConfigValue();
-                ConfigDto databaseConfig = configService.getConfigByName(movePropertyName, event.getGuild().getId());
-                ConfigDto newConfigDto = new ConfigDto(movePropertyName, newDefaultMoveChannel.get(), event.getGuild().getId());
-                if (databaseConfig != null && Objects.equals(databaseConfig.getGuildId(), newConfigDto.getGuildId())) {
-                    configService.updateConfig(newConfigDto);
-                } else {
-                    configService.createNewConfig(newConfigDto);
-                }
-                event.getHook().sendMessageFormat("Set default move channel to '%s'", newDefaultMoveChannel).setEphemeral(true).queue(getConsumer(deleteDelay));
-            }
-        }
-    }
-
-    private void setPrefix(SlashCommandInteractionEvent event, Integer deleteDelay) {
-        String newPrefix = Optional.ofNullable(event.getOption(CONFIG_VALUE)).map(OptionMapping::getAsString).get();
-        newPrefix = newPrefix.length() > 2 ? newPrefix.substring(0, 2) : newPrefix;
-        if (prefixValidation(newPrefix)) {
-            String prefixPropertyName = PREFIX.getConfigValue();
-            ConfigDto databaseConfig = configService.getConfigByName(prefixPropertyName, event.getGuild().getId());
-            ConfigDto newConfigDto = new ConfigDto(prefixPropertyName, newPrefix, event.getGuild().getId());
-
+        String movePropertyName = MOVE.name();
+        Optional<GuildChannelUnion> newDefaultMoveChannelOptional = Optional.ofNullable(event.getOption(movePropertyName)).map(OptionMapping::getAsChannel);
+        if (newDefaultMoveChannelOptional.isPresent()) {
+            ConfigDto databaseConfig = configService.getConfigByName(movePropertyName, event.getGuild().getId());
+            GuildChannelUnion newChannel = newDefaultMoveChannelOptional.get();
+            ConfigDto newConfigDto = new ConfigDto(movePropertyName, newChannel.getName(), event.getGuild().getId());
             if (databaseConfig != null && Objects.equals(databaseConfig.getGuildId(), newConfigDto.getGuildId())) {
                 configService.updateConfig(newConfigDto);
             } else {
                 configService.createNewConfig(newConfigDto);
             }
-            event.getHook().sendMessageFormat("Set prefix to '%s'", newPrefix).setEphemeral(true).queue(getConsumer(deleteDelay));
-
+            event.getHook().sendMessageFormat("Set default move channel to '%s'", newChannel.getName()).setEphemeral(true).queue(getConsumer(deleteDelay));
+        }
+        else {
+            event.getHook().sendMessage("No valid channel was mentioned, so config was not updated").setEphemeral(true).queue(getConsumer(deleteDelay));
         }
     }
 
-
-    private boolean prefixValidation(String newPrefix) {
-        boolean nonAlphanumericPrefix1 = newPrefix.matches("[^a-zA-Z\\d*\\s]");
-        boolean nonAlphanumericPrefix2 = newPrefix.matches("\\W\\S");
-        boolean reservedPrefix = newPrefix.contains("@") || newPrefix.contains("/") || newPrefix.contains(",");
-
-        return (nonAlphanumericPrefix1 || nonAlphanumericPrefix2) && !reservedPrefix;
-    }
 
     @Override
     public String getName() {
@@ -156,14 +101,14 @@ public class SetConfigCommand implements IModerationCommand {
 
     @Override
     public String getDescription() {
-        return "Use this command to set the configuration used for your server";
+        return "Use this command to set the configuration used for this bot your server";
     }
 
     @Override
     public List<OptionData> getOptionData() {
-        OptionData configName = new OptionData(OptionType.STRING, CONFIG_NAME, "What config to adjust for the server", true);
-        OptionData configValue = new OptionData(OptionType.STRING, CONFIG_VALUE, "Value for the config you want to adjust", true);
-        Arrays.stream(values()).forEach(conf -> configName.addChoice(conf.getConfigValue(), conf.getConfigValue()));
-        return List.of(configName, configValue);
+        OptionData defaultVolume = new OptionData(OptionType.INTEGER, VOLUME.name(), "Default volume for audio player on your server (100 without an override)", false);
+        OptionData deleteDelay = new OptionData(OptionType.INTEGER, DELETE_DELAY.name(), "The length of time in seconds before your slash command messages are deleted from the channel they were sent in", false);
+        OptionData channelValue = new OptionData(OptionType.CHANNEL, MOVE.name(), "Value for the default move channel you want if using move command without arguments", false);
+        return List.of(defaultVolume, deleteDelay, channelValue);
     }
 }
---
+++ src/test/java/toby/command/commands/moderation/SetConfigCommandTest.java
@@ -0,0 +1,181 @@
+package toby.command.commands.moderation;
+
+import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import toby.command.CommandContext;
+import toby.command.commands.CommandTest;
+import toby.jpa.dto.ConfigDto;
+import toby.jpa.service.IConfigService;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static toby.jpa.dto.ConfigDto.Configurations.*;
+
+class SetConfigCommandTest implements CommandTest {
+
+    SetConfigCommand setConfigCommand;
+
+    @Mock
+    IConfigService configService;
+
+    @BeforeEach
+    void setUp() {
+        setUpCommonMocks();
+        configService = mock(IConfigService.class);
+        setConfigCommand = new SetConfigCommand(configService);
+    }
+
+    @AfterEach
+    void tearDown() {
+        tearDownCommonMocks();
+        reset(configService);
+    }
+
+    @Test
+    void testSetConfig_notAsServerOwner_sendsErrorMessage() {
+        //Arrange
+        CommandContext commandContext = new CommandContext(event);
+        OptionMapping volumeOptionMapping = mock(OptionMapping.class);
+        when(event.getOption(VOLUME.name())).thenReturn(volumeOptionMapping);
+        when(volumeOptionMapping.getAsInt()).thenReturn(20);
+        when(volumeOptionMapping.getName()).thenReturn(VOLUME.name());
+        when(member.isOwner()).thenReturn(false);
+        when(event.getOptions()).thenReturn(List.of(volumeOptionMapping));
+        when(configService.getConfigByName(VOLUME.name(), "1")).thenReturn(null);
+
+        //Act
+        setConfigCommand.handle(commandContext, requestingUserDto, 0);
+
+        //Assert
+        verify(interactionHook, times(1)).deleteOriginal();
+        verify(configService, times(0)).getConfigByName(VOLUME.name(), "1");
+        verify(configService, times(0)).createNewConfig(any(ConfigDto.class));
+        verify(interactionHook, times(1)).sendMessage(eq("This is currently reserved for the owner of the server only, this may change in future"));
+    }
+
+    @Test
+    void testSetConfig_withOneConfig_createsThatConfig() {
+        //Arrange
+        CommandContext commandContext = new CommandContext(event);
+        OptionMapping volumeOptionMapping = mock(OptionMapping.class);
+        when(event.getOption(VOLUME.name())).thenReturn(volumeOptionMapping);
+        when(volumeOptionMapping.getAsInt()).thenReturn(20);
+        when(volumeOptionMapping.getName()).thenReturn(VOLUME.name());
+        when(member.isOwner()).thenReturn(true);
+        when(event.getOptions()).thenReturn(List.of(volumeOptionMapping));
+        when(configService.getConfigByName(VOLUME.name(), "1")).thenReturn(null);
+
+        //Act
+        setConfigCommand.handle(commandContext, requestingUserDto, 0);
+
+        //Assert
+        verify(interactionHook, times(1)).deleteOriginal();
+        verify(configService, times(1)).getConfigByName(VOLUME.name(), "1");
+        verify(configService, times(1)).createNewConfig(any(ConfigDto.class));
+        verify(interactionHook, times(1)).sendMessageFormat(eq("Set default volume to '%s'"), eq(20));
+    }
+
+    @Test
+    void testSetConfig_withOneConfig_updatesThatConfig() {
+        //Arrange
+        CommandContext commandContext = new CommandContext(event);
+        OptionMapping volumeOptionMapping = mock(OptionMapping.class);
+        when(event.getOption(VOLUME.name())).thenReturn(volumeOptionMapping);
+        when(volumeOptionMapping.getAsInt()).thenReturn(20);
+        when(volumeOptionMapping.getName()).thenReturn(VOLUME.name());
+        when(member.isOwner()).thenReturn(true);
+        when(event.getOptions()).thenReturn(List.of(volumeOptionMapping));
+        ConfigDto dbConfig = mock(ConfigDto.class);
+        when(configService.getConfigByName(VOLUME.name(), "1")).thenReturn(dbConfig);
+        when(dbConfig.getGuildId()).thenReturn("1");
+
+        //Act
+        setConfigCommand.handle(commandContext, requestingUserDto, 0);
+
+        //Assert
+        verify(interactionHook, times(1)).deleteOriginal();
+        verify(configService, times(1)).getConfigByName(VOLUME.name(), "1");
+        verify(configService, times(1)).updateConfig(any(ConfigDto.class));
+        verify(interactionHook, times(1)).sendMessageFormat(eq("Set default volume to '%s'"), eq(20));
+    }
+
+    @Test
+    void testSetConfig_withDeleteDelay_createsThatConfig() {
+        //Arrange
+        CommandContext commandContext = new CommandContext(event);
+        OptionMapping deleteDelayOptionMapping = mock(OptionMapping.class);
+        when(event.getOption(DELETE_DELAY.name())).thenReturn(deleteDelayOptionMapping);
+        when(deleteDelayOptionMapping.getAsInt()).thenReturn(20);
+        when(deleteDelayOptionMapping.getName()).thenReturn(DELETE_DELAY.name());
+        when(member.isOwner()).thenReturn(true);
+        when(event.getOptions()).thenReturn(List.of(deleteDelayOptionMapping));
+        when(configService.getConfigByName(DELETE_DELAY.name(), "1")).thenReturn(null);
+
+        //Act
+        setConfigCommand.handle(commandContext, requestingUserDto, 0);
+
+        //Assert
+        verify(interactionHook, times(1)).deleteOriginal();
+        verify(configService, times(1)).getConfigByName(DELETE_DELAY.name(), "1");
+        verify(configService, times(1)).createNewConfig(any(ConfigDto.class));
+        verify(interactionHook, times(1)).sendMessageFormat(eq("Set default delete message delay for TobyBot music messages to '%d' seconds"), eq(20));
+    }
+
+    @Test
+    void testSetConfig_withMultipleSpecified_createsTwoConfig() {
+        //Arrange
+        CommandContext commandContext = new CommandContext(event);
+        OptionMapping deleteDelayOptionMapping = mock(OptionMapping.class);
+        OptionMapping volumeOptionMapping = mock(OptionMapping.class);
+        when(event.getOption(DELETE_DELAY.name())).thenReturn(deleteDelayOptionMapping);
+        when(deleteDelayOptionMapping.getAsInt()).thenReturn(20);
+        when(deleteDelayOptionMapping.getName()).thenReturn(DELETE_DELAY.name());
+        when(event.getOption(VOLUME.name())).thenReturn(volumeOptionMapping);
+        when(volumeOptionMapping.getAsInt()).thenReturn(20);
+        when(volumeOptionMapping.getName()).thenReturn(VOLUME.name());
+        when(member.isOwner()).thenReturn(true);
+        when(event.getOptions()).thenReturn(List.of(deleteDelayOptionMapping, volumeOptionMapping));
+        when(configService.getConfigByName(DELETE_DELAY.name(), "1")).thenReturn(null);
+        when(configService.getConfigByName(VOLUME.name(), "1")).thenReturn(null);
+
+        //Act
+        setConfigCommand.handle(commandContext, requestingUserDto, 0);
+
+        //Assert
+        verify(interactionHook, times(1)).deleteOriginal();
+        verify(configService, times(1)).getConfigByName(DELETE_DELAY.name(), "1");
+        verify(configService, times(1)).getConfigByName(VOLUME.name(), "1");
+        verify(configService, times(2)).createNewConfig(any(ConfigDto.class));
+        verify(interactionHook, times(1)).sendMessageFormat(eq("Set default volume to '%s'"), eq(20));
+        verify(interactionHook, times(1)).sendMessageFormat(eq("Set default delete message delay for TobyBot music messages to '%d' seconds"), eq(20));
+    }
+
+    @Test
+    void testSetConfig_withMoveChannel_createsThatConfig() {
+        //Arrange
+        CommandContext commandContext = new CommandContext(event);
+        OptionMapping deleteDelayOptionMapping = mock(OptionMapping.class);
+        GuildChannelUnion guildChannelUnion = mock(GuildChannelUnion.class);
+        when(event.getOption(MOVE.name())).thenReturn(deleteDelayOptionMapping);
+        when(deleteDelayOptionMapping.getAsChannel()).thenReturn(guildChannelUnion);
+        when(guildChannelUnion.getName()).thenReturn("Channel Name");
+        when(deleteDelayOptionMapping.getName()).thenReturn(MOVE.name());
+        when(member.isOwner()).thenReturn(true);
+        when(event.getOptions()).thenReturn(List.of(deleteDelayOptionMapping));
+        when(configService.getConfigByName(MOVE.name(), "1")).thenReturn(null);
+
+        //Act
+        setConfigCommand.handle(commandContext, requestingUserDto, 0);
+
+        //Assert
+        verify(interactionHook, times(1)).deleteOriginal();
+        verify(configService, times(1)).getConfigByName(MOVE.name(), "1");
+        verify(configService, times(1)).createNewConfig(any(ConfigDto.class));
+        verify(interactionHook, times(1)).sendMessageFormat(eq("Set default move channel to '%s'"), eq("Channel Name"));
+    }
+}
