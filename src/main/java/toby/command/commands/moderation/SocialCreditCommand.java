1c1ae3f30f720929b2569747d6c866fcc821b02a
Matthew Layton <15813613+ml404@users.noreply.github.com>
2023-10-25 18:39:59 +0100

    finish moderation unit tests

--- src/main/java/toby/command/commands/moderation/SocialCreditCommand.java
+++ src/main/java/toby/command/commands/moderation/SocialCreditCommand.java
@@ -1,7 +1,8 @@
 package toby.command.commands.moderation;
 
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Mentions;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -35,72 +36,76 @@ public class SocialCreditCommand implements IModerationCommand {
         event.deferReply().queue();
         deleteAfter(event.getHook(), deleteDelay);
         final Member member = ctx.getMember();
-        if(!event.getGuild().isLoaded()) event.getGuild().loadMembers();
+        if (!event.getGuild().isLoaded()) event.getGuild().loadMembers();
         if (Optional.ofNullable(event.getOption(LEADERBOARD)).map(OptionMapping::getAsBoolean).orElse(false)) {
-            Map<Long, Long> discordSocialCreditMap = new HashMap<>();
-            userService.listGuildUsers(event.getGuild().getIdLong()).forEach(userDto -> {
-                Long socialCredit = userDto.getSocialCredit() == null ? 0L : userDto.getSocialCredit();
-                discordSocialCreditMap.put(userDto.getDiscordId(), socialCredit);
-            });
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("**Social Credit Leaderboard**\n");
-            stringBuilder.append("**-----------------------------**\n");
-            LinkedHashMap<Long, Long> sortedMap = discordSocialCreditMap.entrySet()
-                    .stream()
-                    .sorted(comparingByValue(Comparator.reverseOrder()))
-                    .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
-            AtomicInteger position = new AtomicInteger();
-            sortedMap.forEach((k, v) -> {
-                position.getAndIncrement();
-                event.getGuild()
-                        .getMembers()
-                        .stream()
-                        .filter(member1 -> member1.getIdLong() == k)
-                        .findFirst()
-                        .ifPresent(memberById -> stringBuilder.append(String.format("#%s: %s - score: %d\n", position, memberById.getEffectiveName(), v)));
-            });
-            event.getHook().sendMessageFormat(stringBuilder.toString()).setEphemeral(true).queue(getConsumer(deleteDelay));
-        } else {
-            List<Member> mentionedMembers = Optional.ofNullable(event.getOption(USERS)).map(OptionMapping::getMentions).map(Mentions::getMembers).orElse(Collections.emptyList());
-            if (mentionedMembers.isEmpty()) {
-                listSocialCreditScore(requestingUserDto, member.getEffectiveName(), deleteDelay, event);
-            } else
-                mentionedMembers.forEach(targetMember ->
-                {
-                    UserDto targetUserDto = userService.getUserById(targetMember.getIdLong(), targetMember.getGuild().getIdLong());
-                    if (event.getOptions().stream().noneMatch(optionMapping -> optionMapping.getAsMember().getUser().getAsMention().isEmpty())) {
-                        listSocialCreditScore(targetUserDto, targetMember.getEffectiveName(), deleteDelay, event);
-                    } else {
-                        //Check to see if the database contained an entry for the user we have made a request against
-                        if (targetUserDto != null) {
-                            boolean isSameGuild = requestingUserDto.getGuildId().equals(targetUserDto.getGuildId());
-                            boolean requesterCanAdjustPermissions = member.isOwner();
-                            if (requesterCanAdjustPermissions && isSameGuild) {
-                                long socialCreditAdjustment = Optional.ofNullable(event.getOption(SOCIAL_CREDIT)).map(OptionMapping::getAsLong).orElse(0L);
-                                UserDto updatedUser = validateArgumentsAndAdjustSocialCredit(targetUserDto, event, socialCreditAdjustment, ctx.getMember().isOwner(), deleteDelay);
-                                event.getHook().sendMessageFormat("Updated user %s's social credit by %d. New score is: %d", targetMember.getEffectiveName(), socialCreditAdjustment, updatedUser.getSocialCredit()).setEphemeral(true).queue(getConsumer(deleteDelay));
-                            } else
-                                event.getHook().sendMessageFormat("User '%s' is not allowed to adjust the social credit of user '%s'.", member.getEffectiveName(), targetMember.getEffectiveName()).setEphemeral(true).queue(getConsumer(deleteDelay));
-                        }
-                    }
-                });
+            createAndPrintLeaderboard(event, deleteDelay);
+            return;
         }
+        calculateAndUpdateSocialCredit(ctx, event, requestingUserDto, member, deleteDelay);
 
     }
 
-    private void listSocialCreditScore(UserDto userDto, String mentionedName, Integer deleteDelay, SlashCommandInteractionEvent event) {
+    private void calculateAndUpdateSocialCredit(CommandContext ctx, SlashCommandInteractionEvent event, UserDto requestingUserDto, Member requestingMember, Integer deleteDelay) {
+        Optional<User> optionalUser = Optional.ofNullable(event.getOption(USERS)).map(OptionMapping::getAsUser);
+        if (optionalUser.isEmpty()) {
+            listSocialCreditScore(event, requestingUserDto, requestingMember.getEffectiveName(), deleteDelay);
+            return;
+        }
+        User user = optionalUser.get();
+        //Check to see if the database contained an entry for the user we have made a request against
+        UserDto targetUserDto = userService.getUserById(user.getIdLong(), requestingUserDto.getGuildId());
+        if (targetUserDto != null) {
+            boolean isSameGuild = requestingUserDto.getGuildId().equals(targetUserDto.getGuildId());
+            if (requestingMember.isOwner() && isSameGuild) {
+                Optional<Long> scOptional = Optional.ofNullable(event.getOption(SOCIAL_CREDIT)).map(OptionMapping::getAsLong);
+                if (scOptional.isPresent()) {
+                    UserDto updatedUser = updateUserSocialCredit(targetUserDto, scOptional.get());
+                    event.getHook().sendMessageFormat("Updated user %s's social credit by %d. New score is: %d", user.getEffectiveName(), scOptional.get(), updatedUser.getSocialCredit()).setEphemeral(true).queue(getConsumer(deleteDelay));
+                } else {
+                    listSocialCreditScore(event, targetUserDto, user.getEffectiveName(), deleteDelay);
+                }
+            } else
+                event.getHook().sendMessageFormat("User '%s' is not allowed to adjust the social credit of user '%s'.", requestingMember.getEffectiveName(), user.getEffectiveName()).setEphemeral(true).queue(getConsumer(deleteDelay));
+        }
+    }
+
+
+    private void createAndPrintLeaderboard(SlashCommandInteractionEvent event, Integer deleteDelay) {
+        Map<Long, Long> discordSocialCreditMap = new HashMap<>();
+        Guild guild = event.getGuild();
+        userService.listGuildUsers(guild.getIdLong()).forEach(userDto -> {
+            Long socialCredit = userDto.getSocialCredit() == null ? 0L : userDto.getSocialCredit();
+            discordSocialCreditMap.put(userDto.getDiscordId(), socialCredit);
+        });
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("**Social Credit Leaderboard**\n");
+        stringBuilder.append("**-----------------------------**\n");
+        LinkedHashMap<Long, Long> sortedMap = discordSocialCreditMap
+                .entrySet()
+                .stream()
+                .sorted(comparingByValue(Comparator.reverseOrder()))
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
+        AtomicInteger position = new AtomicInteger();
+        sortedMap.forEach((k, v) -> {
+            position.getAndIncrement();
+            guild.getMembers()
+                    .stream()
+                    .filter(member1 -> member1.getIdLong() == k)
+                    .findFirst()
+                    .ifPresent(memberById -> stringBuilder.append(String.format("#%s: %s - score: %d\n", position, memberById.getEffectiveName(), v)));
+        });
+        event.getHook().sendMessageFormat(stringBuilder.toString()).setEphemeral(true).queue(getConsumer(deleteDelay));
+    }
+
+    private void listSocialCreditScore(SlashCommandInteractionEvent event, UserDto userDto, String mentionedName, Integer deleteDelay) {
         Long socialCredit = userDto.getSocialCredit() == null ? 0L : userDto.getSocialCredit();
         event.getHook().sendMessageFormat("%s's social credit is: %d", mentionedName, socialCredit).setEphemeral(true).queue(getConsumer(deleteDelay));
     }
 
-    private UserDto validateArgumentsAndAdjustSocialCredit(UserDto targetUserDto, SlashCommandInteractionEvent event, Long socialCreditScore, boolean isOwner, Integer deleteDelay) {
-        if (isOwner) {
-            Long socialCredit = targetUserDto.getSocialCredit() == null ? 0L : targetUserDto.getSocialCredit();
-            targetUserDto.setSocialCredit(socialCredit + socialCreditScore);
-            userService.updateUser(targetUserDto);
-            return targetUserDto;
-        } else
-            sendErrorMessage(event, deleteDelay);
+    private UserDto updateUserSocialCredit(UserDto targetUserDto, Long socialCreditScore) {
+        Long socialCredit = targetUserDto.getSocialCredit() == null ? 0L : targetUserDto.getSocialCredit();
+        targetUserDto.setSocialCredit(socialCredit + socialCreditScore);
+        userService.updateUser(targetUserDto);
         return targetUserDto;
     }
 
@@ -118,7 +123,7 @@ public class SocialCreditCommand implements IModerationCommand {
     @Override
     public List<OptionData> getOptionData() {
         OptionData leaderboard = new OptionData(OptionType.BOOLEAN, LEADERBOARD, "Show the leaderboard");
-        OptionData users = new OptionData(OptionType.STRING, USERS, "User(s) to adjust the social credit value. Without a value will display their social credit amount");
+        OptionData users = new OptionData(OptionType.USER, USERS, "User(s) to adjust the social credit value. Without a value will display their social credit amount");
         OptionData creditAmount = new OptionData(OptionType.NUMBER, SOCIAL_CREDIT, "Score to add or deduct from mentioned user's social credit");
         return List.of(users, creditAmount, leaderboard);
     }
