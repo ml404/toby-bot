322f29e977825026753ce4d051e5836726380fc9
Matthew Layton <15813613+ml404@users.noreply.github.com>
2024-06-19 14:45:09 +0100

    fix join command test

--- src/main/kotlin/toby/command/commands/music/JoinCommand.kt
+++ src/main/kotlin/toby/command/commands/music/JoinCommand.kt
@@ -11,6 +11,7 @@ import toby.jpa.service.IConfigService
 import toby.lavaplayer.PlayerManager
 
 class JoinCommand(private val configService: IConfigService) : IMusicCommand {
+
     override fun handle(ctx: CommandContext, requestingUserDto: UserDto, deleteDelay: Int?) {
         handleMusicCommand(ctx, PlayerManager.instance, requestingUserDto, deleteDelay)
     }
@@ -18,43 +19,56 @@ class JoinCommand(private val configService: IConfigService) : IMusicCommand {
     override fun handleMusicCommand(ctx: CommandContext, instance: PlayerManager, requestingUserDto: UserDto, deleteDelay: Int?) {
         val event = ctx.event
         event.deferReply().queue()
-        val self = ctx.selfMember
+
+        val self = ctx.selfMember ?: return
+        val memberVoiceState = doJoinChannelValidation(ctx, deleteDelay) ?: return
+        val audioManager = event.guild!!.audioManager
+        val memberChannel: AudioChannel? = memberVoiceState.channel
+
         if (!requestingUserDto.musicPermission) {
             sendErrorMessage(event, deleteDelay!!)
             return
         }
-        val memberVoiceState = doJoinChannelValidation(ctx, deleteDelay) ?: return
-        val audioManager = event.guild!!.audioManager
-        val memberChannel: AudioChannel? = memberVoiceState.channel
-        if (self!!.hasPermission(Permission.VOICE_CONNECT)) {
-            audioManager.openAudioConnection(memberChannel)
-            val volumePropertyName = ConfigDto.Configurations.VOLUME.configValue
-            val databaseConfig = configService.getConfigByName(volumePropertyName, event.guild!!.id)
-            val defaultVolume = databaseConfig?.value?.toInt() ?: 100
-            instance.getMusicManager(event.guild!!).audioPlayer.volume = defaultVolume
-            event.hook.sendMessageFormat("Connecting to `\uD83D\uDD0A %s` with volume '%s'", memberChannel!!.name, defaultVolume).queue(invokeDeleteOnMessageResponse(deleteDelay!!))
+
+        if (!self.hasPermission(Permission.VOICE_CONNECT)) {
+            sendErrorMessage(event, deleteDelay!!)
+            return
         }
+
+        audioManager.openAudioConnection(memberChannel)
+        val volumePropertyName = ConfigDto.Configurations.VOLUME.configValue
+        val databaseConfig = configService.getConfigByName(volumePropertyName, event.guild?.id)
+        val defaultVolume = databaseConfig?.value?.toInt() ?: 100
+        instance.getMusicManager(event.guild!!).audioPlayer.volume = defaultVolume
+
+        event.hook.sendMessage("Connecting to `\uD83D\uDD0A ${memberChannel!!.name}` with volume '$defaultVolume'").queue(invokeDeleteOnMessageResponse(deleteDelay!!))
     }
 
     private fun doJoinChannelValidation(ctx: CommandContext, deleteDelay: Int?): GuildVoiceState? {
-        val self = ctx.selfMember
-        val selfVoiceState = self!!.voiceState
+        val self = ctx.selfMember ?: return null
+        val selfVoiceState = self.voiceState
         val event = ctx.event
-        if (selfVoiceState!!.inAudioChannel()) {
+
+        if (selfVoiceState?.inAudioChannel() == true) {
             event.hook.sendMessage("I'm already in a voice channel").setEphemeral(true).queue(invokeDeleteOnMessageResponse(deleteDelay!!))
             return null
         }
-        val member = ctx.member
-        val memberVoiceState = member!!.voiceState
-        if (!memberVoiceState!!.inAudioChannel()) {
+
+        val member = ctx.member ?: return null
+        val memberVoiceState = member.voiceState
+
+        if (memberVoiceState?.inAudioChannel() != true) {
             event.hook.sendMessage("You need to be in a voice channel for this command to work").setEphemeral(true).queue(invokeDeleteOnMessageResponse(deleteDelay!!))
             return null
         }
+
         return memberVoiceState
     }
 
-    override val name: String
-        get() = "join"
-    override val description: String
-        get() = "Makes the bot join your voice channel"
+    override val name: String = "join"
+    override val description: String = "Makes the bot join your voice channel"
+
+    private fun sendErrorMessage(ctx: CommandContext, deleteDelay: Int) {
+        ctx.event.hook.sendMessage("You aren't allowed to use this command kid").setEphemeral(true).queue(invokeDeleteOnMessageResponse(deleteDelay))
+    }
 }
--- src/test/kotlin/toby/command/commands/music/JoinCommandTest.kt
+++ src/test/kotlin/toby/command/commands/music/JoinCommandTest.kt
@@ -1,19 +1,29 @@
 package toby.command.commands.music
 
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack
-import io.mockk.*
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import net.dv8tion.jda.api.Permission
-import net.dv8tion.jda.api.entities.GuildVoiceState
-import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import toby.command.CommandContext
-import toby.command.CommandTest
+import toby.command.CommandTest.Companion.event
+import toby.command.CommandTest.Companion.guild
+import toby.command.CommandTest.Companion.member
+import toby.command.CommandTest.Companion.requestingUserDto
+import toby.command.commands.music.MusicCommandTest.Companion.audioChannelUnion
+import toby.command.commands.music.MusicCommandTest.Companion.audioManager
 import toby.command.commands.music.MusicCommandTest.Companion.audioPlayer
+import toby.command.commands.music.MusicCommandTest.Companion.botMember
+import toby.command.commands.music.MusicCommandTest.Companion.memberVoiceState
+import toby.command.commands.music.MusicCommandTest.Companion.musicManager
 import toby.command.commands.music.MusicCommandTest.Companion.playerManager
 import toby.command.commands.music.MusicCommandTest.Companion.track
 import toby.command.commands.music.MusicCommandTest.Companion.trackScheduler
+import toby.jpa.dto.ConfigDto
 import toby.jpa.service.IConfigService
 import java.util.concurrent.ArrayBlockingQueue
 
@@ -26,6 +36,7 @@ internal class JoinCommandTest : MusicCommandTest {
         setupCommonMusicMocks()
         configService = mockk<IConfigService>()
         command = JoinCommand(configService)
+        every { configService.getConfigByName(any(), any()) } returns ConfigDto("", "100", "1")
     }
 
     @AfterEach
@@ -37,35 +48,31 @@ internal class JoinCommandTest : MusicCommandTest {
     @Test
     fun test_joinCommand() {
         setUpAudioChannelsWithBotNotInChannel()
-        val commandContext = CommandContext(CommandTest.event)
+        val commandContext = CommandContext(event)
 
         every { audioPlayer.isPaused } returns false
         every { audioPlayer.playingTrack } returns track
         every { playerManager.isCurrentlyStoppable } returns false
-        every { MusicCommandTest.memberVoiceState.channel } returns MusicCommandTest.audioChannelUnion
-        every { MusicCommandTest.audioChannelUnion.name } returns "Channel Name"
+        every { memberVoiceState.channel } returns audioChannelUnion
+        every { audioChannelUnion.name } returns "Channel Name"
         val queue: ArrayBlockingQueue<AudioTrack?> = mockk()
         every { trackScheduler.queue } returns queue
-        every { CommandTest.member.voiceState } returns MusicCommandTest.memberVoiceState
-        every { MusicCommandTest.memberVoiceState.inAudioChannel() } returns true
-        every { MusicCommandTest.botMember.hasPermission(Permission.VOICE_CONNECT) } returns true
+        every { member.voiceState } returns memberVoiceState
+        every { memberVoiceState.inAudioChannel() } returns true
+        every { botMember.hasPermission(Permission.VOICE_CONNECT) } returns true
 
         // Act
         command.handleMusicCommand(
             commandContext,
             playerManager,
-            CommandTest.requestingUserDto,
+            requestingUserDto,
             0
         )
 
         // Assert
-        verify(exactly = 1) { MusicCommandTest.audioManager.openAudioConnection(MusicCommandTest.audioChannelUnion) }
-        verify(exactly = 1) { playerManager.getMusicManager(CommandTest.guild) }
-        verify(exactly = 1) { MusicCommandTest.musicManager.audioPlayer.volume = 100 }
-        verify(exactly = 1) { CommandTest.event.hook.sendMessageFormat(
-            eq("Connecting to `\uD83D\uDD0A %s` with volume '%s'"),
-            eq("Channel Name"),
-            eq(100)
-        ) }
+        verify(exactly = 1) { audioManager.openAudioConnection(audioChannelUnion) }
+        verify(exactly = 1) { playerManager.getMusicManager(guild) }
+        verify(exactly = 1) { audioPlayer.volume = 100 }
+        verify(exactly = 1) { event.hook.sendMessage(eq("Connecting to `\uD83D\uDD0A Channel Name` with volume '100'")) }
     }
 }
