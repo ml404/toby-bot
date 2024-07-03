848409030be95bdcb5282521b37e952a7257b4e4
Matthew Layton <15813613+ml404@users.noreply.github.com>
2024-07-03 13:04:35 +0100

    fix join command test

--- src/test/kotlin/toby/command/commands/music/IntroSongCommandTest.kt
+++ src/test/kotlin/toby/command/commands/music/IntroSongCommandTest.kt
@@ -13,7 +13,10 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import toby.command.CommandContext
-import toby.command.CommandTest
+import toby.command.CommandTest.Companion.event
+import toby.command.CommandTest.Companion.guild
+import toby.command.CommandTest.Companion.interactionHook
+import toby.command.CommandTest.Companion.member
 import toby.command.CommandTest.Companion.requestingUserDto
 import toby.jpa.dto.ConfigDto
 import toby.jpa.dto.MusicDto
@@ -28,21 +31,32 @@ import java.util.concurrent.ExecutionException
 
 internal class IntroSongCommandTest : MusicCommandTest {
     lateinit var introSongCommand: IntroSongCommand
-
     lateinit var userService: IUserService
     lateinit var musicFileService: IMusicFileService
     lateinit var configService: IConfigService
-
     lateinit var mentionedUserDto: UserDto
 
     @BeforeEach
     fun setUp() {
         setupCommonMusicMocks()
-        userService = mockk()
-        musicFileService = mockk()
+        userService = mockk(relaxed = true)
+        musicFileService = mockk(relaxed = true)
         configService = mockk()
         introSongCommand = IntroSongCommand(userService, musicFileService, configService)
         mentionedUserDto = requestingUserDto
+
+        every { event.getOption("volume") } returns mockk {
+            every { asInt } returns 10
+        }
+        every { event.getOption("link") } returns mockk {
+            every { asString } returns "https://www.youtube.com/"
+        }
+        every { event.getOption("users")?.mentions } returns mockk {
+            every { members } returns emptyList()
+        }
+        every { event.getOption("attachment") } returns mockk {
+            every { asAttachment } returns mockk(relaxed = true)
+        }
     }
 
     @AfterEach
@@ -54,14 +68,10 @@ internal class IntroSongCommandTest : MusicCommandTest {
     @Test
     fun testIntroSong_withSuperuser_andValidLinkAttached_setsIntroViaUrl() {
         // Arrange
-        val commandContext = CommandContext(CommandTest.event)
-        val linkOptionMapping = mockk<OptionMapping>()
-        val userOptionMapping = mockk<OptionMapping>()
+        val commandContext = CommandContext(event)
         val volumeConfig = ConfigDto("DEFAULT_VOLUME", "20", "1")
 
-        every { CommandTest.event.getOption("users") } returns userOptionMapping
-        every { CommandTest.event.getOption("link") } returns linkOptionMapping
-        every { linkOptionMapping.asString } returns "https://www.youtube.com/"
+        
         every { userService.listGuildUsers(1L) } returns listOf(requestingUserDto)
         every { configService.getConfigByName("DEFAULT_VOLUME", "1") } returns volumeConfig
 
@@ -75,24 +85,23 @@ internal class IntroSongCommandTest : MusicCommandTest {
 
         // Assert
         verify { musicFileService.createNewMusicFile(any()) }
-        verify { CommandTest.interactionHook.sendMessageFormat(
-            eq("Successfully set %s's intro song to '%s' with volume '%d'"),
-            eq("UserName"),
-            eq("https://www.youtube.com/"),
-            eq(20)
-        ) }
+        verify {
+            interactionHook.sendMessageFormat(
+                eq("Successfully set %s's intro song to '%s' with volume '%d'"),
+                eq("UserName"),
+                eq("https://www.youtube.com/"),
+                eq(20)
+            )
+        }
     }
 
     @Test
     fun testIntroSong_withSuperuser_andValidLinkAttachedWithExistingMusicFile_setsIntroViaUrl() {
         // Arrange
-        val commandContext = CommandContext(CommandTest.event)
+        val commandContext = CommandContext(event)
         val linkOptionMapping = mockk<OptionMapping>()
-        val userOptionMapping = mockk<OptionMapping>()
 
-        every { CommandTest.event.getOption("users") } returns userOptionMapping
-        every { CommandTest.event.getOption("link") } returns linkOptionMapping
-        every { linkOptionMapping.asString } returns "https://www.youtube.com/"
+        
         every { userService.listGuildUsers(1L) } returns listOf(requestingUserDto)
         every { configService.getConfigByName("DEFAULT_VOLUME", "1") } returns ConfigDto("DEFAULT_VOLUME", "20", "1")
         every { requestingUserDto.musicDto } returns MusicDto(1L, 1L, "filename", 20, null)
@@ -107,24 +116,25 @@ internal class IntroSongCommandTest : MusicCommandTest {
 
         // Assert
         verify { musicFileService.updateMusicFile(any()) }
-        verify { CommandTest.interactionHook.sendMessageFormat(
-            eq("Successfully updated %s's intro song to '%s' with volume '%d'"),
-            eq("UserName"),
-            eq("https://www.youtube.com/"),
-            eq(20)
-        ) }
+        verify {
+            interactionHook.sendMessageFormat(
+                eq("Successfully updated %s's intro song to '%s' with volume '%d'"),
+                eq("UserName"),
+                eq("https://www.youtube.com/"),
+                eq(20)
+            )
+        }
     }
 
     @Test
     fun testIntroSong_withSuperuser_andMentionedMembers_setsMentionedMembersIntroViaUrl() {
         // Arrange
-        val commandContext = CommandContext(CommandTest.event)
+        val commandContext = CommandContext(event)
         val linkOptionMapping = mockk<OptionMapping>()
         val userOptionMapping = mockk<OptionMapping>()
 
-        every { CommandTest.event.getOption("users") } returns userOptionMapping
-        every { CommandTest.event.getOption("link") } returns linkOptionMapping
-        every { linkOptionMapping.asString } returns "https://www.youtube.com/"
+        every { event.getOption("users") } returns userOptionMapping
+        
         every { userService.createNewUser(any()) } returns mentionedUserDto
 
         setupMentions(userOptionMapping)
@@ -139,31 +149,31 @@ internal class IntroSongCommandTest : MusicCommandTest {
 
         // Assert
         verify { musicFileService.createNewMusicFile(any()) }
-        verify { CommandTest.interactionHook.sendMessageFormat(
-            eq("Successfully set %s's intro song to '%s' with volume '%d'"),
-            eq("Another Username"),
-            eq("https://www.youtube.com/"),
-            eq(20)
-        ) }
+        verify {
+            interactionHook.sendMessageFormat(
+                eq("Successfully set %s's intro song to '%s' with volume '%d'"),
+                eq("Another Username"),
+                eq("https://www.youtube.com/"),
+                eq(20)
+            )
+        }
     }
 
     @Test
     fun testIntroSong_withoutPermissionsAndSomeoneMentioned_andValidLinkAttached_doesNotSetIntroViaUrl() {
         // Arrange
-        val commandContext = CommandContext(CommandTest.event)
-        val linkOptionMapping = mockk<OptionMapping>()
+        val commandContext = CommandContext(event)
         val userOptionMapping = mockk<OptionMapping>()
         val mentions = mockk<Mentions>()
 
-        every { CommandTest.event.getOption("users") } returns userOptionMapping
+        every { event.getOption("users") } returns userOptionMapping
         every { userOptionMapping.mentions } returns mentions
-        every { mentions.members } returns listOf(CommandTest.member)
-        every { CommandTest.event.getOption("link") } returns linkOptionMapping
-        every { linkOptionMapping.asString } returns "https://www.youtube.com/"
+        every { mentions.members } returns listOf(member)
+        
         every { userService.listGuildUsers(1L) } returns listOf(requestingUserDto)
         every { configService.getConfigByName("DEFAULT_VOLUME", "1") } returns ConfigDto("DEFAULT_VOLUME", "20", "1")
-        every { CommandTest.guild.owner } returns CommandTest.member
-        every { CommandTest.member.effectiveName } returns "Effective Name"
+        every { guild.owner } returns member
+        every { member.effectiveName } returns "Effective Name"
         every { requestingUserDto.superUser } returns false
 
         // Act
@@ -176,7 +186,7 @@ internal class IntroSongCommandTest : MusicCommandTest {
 
         // Assert
         verify {
-            CommandTest.interactionHook.sendMessageFormat(
+            interactionHook.sendMessageFormat(
                 eq("You do not have adequate permissions to use this command, if you believe this is a mistake talk to Effective Name")
             )
         }
@@ -186,12 +196,10 @@ internal class IntroSongCommandTest : MusicCommandTest {
     @Throws(ExecutionException::class, InterruptedException::class, IOException::class)
     fun testIntroSong_withSuperuser_andValidAttachment_setsIntroViaAttachment_andCreatesNewMusicFile() {
         // Arrange
-        val commandContext = CommandContext(CommandTest.event)
+        val commandContext = CommandContext(event)
         val attachmentOptionMapping = mockk<OptionMapping>()
-        val userOptionMapping = mockk<OptionMapping>()
 
-        every { CommandTest.event.getOption("users") } returns userOptionMapping
-        every { CommandTest.event.getOption("attachment") } returns attachmentOptionMapping
+        every { event.getOption("attachment") } returns attachmentOptionMapping
         setupAttachments(attachmentOptionMapping, "mp3", 1000)
         every { userService.listGuildUsers(1L) } returns listOf(requestingUserDto)
         every { configService.getConfigByName("DEFAULT_VOLUME", "1") } returns ConfigDto("DEFAULT_VOLUME", "20", "1")
@@ -207,24 +215,26 @@ internal class IntroSongCommandTest : MusicCommandTest {
         // Assert
         verify { musicFileService.createNewMusicFile(any()) }
         verify { userService.updateUser(eq(requestingUserDto)) }
-        verify { CommandTest.interactionHook.sendMessageFormat(
-            eq("Successfully set %s's intro song to '%s' with volume '%d'"),
-            eq("UserName"),
-            eq("filename"),
-            eq(20)
-        ) }
+        verify {
+            interactionHook.sendMessageFormat(
+                eq("Successfully set %s's intro song to '%s' with volume '%d'"),
+                eq("UserName"),
+                eq("filename"),
+                eq(20)
+            )
+        }
     }
 
     @Test
     @Throws(ExecutionException::class, InterruptedException::class, IOException::class)
     fun testIntroSong_withSuperuser_andMentionedMembers_setsIntroViaAttachment() {
         // Arrange
-        val commandContext = CommandContext(CommandTest.event)
+        val commandContext = CommandContext(event)
         val attachmentOptionMapping = mockk<OptionMapping>()
         val userOptionMapping = mockk<OptionMapping>()
 
-        every { CommandTest.event.getOption("users") } returns userOptionMapping
-        every { CommandTest.event.getOption("attachment") } returns attachmentOptionMapping
+        every { event.getOption("users") } returns userOptionMapping
+        every { event.getOption("attachment") } returns attachmentOptionMapping
         setupAttachments(attachmentOptionMapping, "mp3", 1000)
         every { userService.listGuildUsers(1L) } returns listOf(requestingUserDto)
         every { userService.createNewUser(any()) } returns mentionedUserDto
@@ -243,12 +253,14 @@ internal class IntroSongCommandTest : MusicCommandTest {
         // Assert
         verify { musicFileService.createNewMusicFile(any()) }
         verify { userService.updateUser(eq(mentionedUserDto)) }
-        verify { CommandTest.interactionHook.sendMessageFormat(
-            eq("Successfully set %s's intro song to '%s' with volume '%d'"),
-            eq("Another Username"),
-            eq("filename"),
-            eq(20)
-        ) }
+        verify {
+            interactionHook.sendMessageFormat(
+                eq("Successfully set %s's intro song to '%s' with volume '%d'"),
+                eq("Another Username"),
+                eq("filename"),
+                eq(20)
+            )
+        }
     }
 
     private fun setupMentions(userOptionMapping: OptionMapping) {
