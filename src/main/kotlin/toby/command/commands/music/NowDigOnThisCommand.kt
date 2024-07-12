b3b9ba43aa12114ec68474896b38b945331d8b90
Matthew Layton <15813613+ml404@users.noreply.github.com>
2024-07-12 11:13:21 +0100

    fix NDOT and JsonParserTest

--- src/main/kotlin/toby/command/commands/music/NowDigOnThisCommand.kt
+++ src/main/kotlin/toby/command/commands/music/NowDigOnThisCommand.kt
@@ -1,7 +1,6 @@
 package toby.command.commands.music
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
-import net.dv8tion.jda.api.interactions.commands.OptionMapping
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
 import toby.command.CommandContext
@@ -10,12 +9,12 @@ import toby.helpers.MusicPlayerHelper
 import toby.helpers.URLHelper
 import toby.jpa.dto.UserDto
 import toby.lavaplayer.PlayerManager
-import java.util.*
 
 class NowDigOnThisCommand : IMusicCommand {
     private val LINK = "link"
     private val START_POSITION = "start"
     private val VOLUME = "volume"
+
     override fun handle(ctx: CommandContext, requestingUserDto: UserDto, deleteDelay: Int?) {
         handleMusicCommand(ctx, PlayerManager.instance, requestingUserDto, deleteDelay)
     }
@@ -23,23 +22,36 @@ class NowDigOnThisCommand : IMusicCommand {
     override fun handleMusicCommand(ctx: CommandContext, instance: PlayerManager, requestingUserDto: UserDto, deleteDelay: Int?) {
         val event = ctx.event
         event.deferReply().queue()
-        if (requestingUserDto.digPermission) {
-            val linkOptional = Optional.ofNullable(event.getOption(LINK)).map { obj: OptionMapping -> obj.asString }
-            if (linkOptional.isEmpty) {
-                event.hook.sendMessageFormat("Correct usage is `%snowdigonthis <youtube linkOptional>`", "/").queue(invokeDeleteOnMessageResponse(deleteDelay!!))
-                return
-            }
-            if (IMusicCommand.isInvalidChannelStateForCommand(ctx, deleteDelay)) return
-            var link = linkOptional.get()
-            if (link.contains("youtube") && !URLHelper.isValidURL(link)) link = "ytsearch:$linkOptional"
-            val startPosition = MusicPlayerHelper.adjustTrackPlayingTimes(Optional.ofNullable(event.getOption(START_POSITION)).map { obj: OptionMapping -> obj.asLong }.orElse(0L))
-            val musicManager = instance.getMusicManager(ctx.guild)
-            val volume = Optional.ofNullable(event.getOption(VOLUME)).map { obj: OptionMapping -> obj.asInt }.orElse(musicManager.audioPlayer.volume)
-            if (musicManager.scheduler.queue.isEmpty()) {
-                musicManager.audioPlayer.volume = volume
-            }
-            instance.loadAndPlay(ctx.guild, event, link, false, deleteDelay ?: 0, startPosition, volume)
-        } else sendErrorMessage(event, deleteDelay)
+
+        if (!requestingUserDto.digPermission) {
+            sendErrorMessage(event, deleteDelay)
+            return
+        }
+
+        val linkOption = event.getOption(LINK)?.asString
+        if (linkOption.isNullOrBlank()) {
+            event.hook
+                .sendMessage("Correct usage is `/nowdigonthis <youtube link>`")
+                .queue(invokeDeleteOnMessageResponse(deleteDelay ?: 0))
+            return
+        }
+
+        if (IMusicCommand.isInvalidChannelStateForCommand(ctx, deleteDelay)) return
+
+        var link = linkOption
+        if (link.contains("youtube") && !URLHelper.isValidURL(link)) {
+            link = "ytsearch:$link"
+        }
+
+        val startPosition = MusicPlayerHelper.adjustTrackPlayingTimes(event.getOption(START_POSITION)?.asLong ?: 0L)
+        val musicManager = instance.getMusicManager(ctx.guild)
+        val volume = event.getOption(VOLUME)?.asInt ?: musicManager.audioPlayer.volume
+
+        if (musicManager.scheduler.queue.isEmpty()) {
+            musicManager.audioPlayer.volume = volume
+        }
+
+        instance.loadAndPlay(ctx.guild, event, link, false, deleteDelay ?: 0, startPosition, volume)
     }
 
     override val name: String get() = "nowdigonthis"
@@ -47,15 +59,16 @@ class NowDigOnThisCommand : IMusicCommand {
 
     override fun getErrorMessage(name: String?): String = "I'm gonna put some dirt in your eye $name"
 
-    fun sendErrorMessage(event: SlashCommandInteractionEvent, deleteDelay: Int?) {
-        event.hook.sendMessage(getErrorMessage(event.member!!.effectiveName)).queue(invokeDeleteOnMessageResponse(deleteDelay!!))
+    private fun sendErrorMessage(event: SlashCommandInteractionEvent, deleteDelay: Int?) {
+        event.hook.sendMessage(getErrorMessage(event.member!!.effectiveName))
+            .queue(invokeDeleteOnMessageResponse(deleteDelay ?: 0))
     }
 
     override val optionData: List<OptionData>
         get() {
             val linkArg = OptionData(OptionType.STRING, LINK, "Link to play that cannot be stopped unless requested by a super user", true)
             val startPositionArg = OptionData(OptionType.NUMBER, START_POSITION, "Start position of the track in seconds")
-            val volume = OptionData(OptionType.INTEGER, VOLUME, "Volume to play at")
-            return listOf(linkArg, volume, startPositionArg)
+            val volumeArg = OptionData(OptionType.INTEGER, VOLUME, "Volume to play at")
+            return listOf(linkArg, volumeArg, startPositionArg)
         }
 }
--- src/test/kotlin/toby/command/commands/music/NowDigOnThisCommandTest.kt
+++ src/test/kotlin/toby/command/commands/music/NowDigOnThisCommandTest.kt
@@ -2,14 +2,16 @@ package toby.command.commands.music
 
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo
+import io.mockk.*
 import net.dv8tion.jda.api.interactions.commands.OptionMapping
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.ArgumentMatchers
-import org.mockito.Mockito
 import toby.command.CommandContext
 import toby.command.CommandTest
+import toby.command.CommandTest.Companion.event
+import toby.command.CommandTest.Companion.guild
+import toby.command.commands.music.MusicCommandTest.Companion.audioPlayer
 import toby.command.commands.music.MusicCommandTest.Companion.musicManager
 import toby.command.commands.music.MusicCommandTest.Companion.playerManager
 import toby.command.commands.music.MusicCommandTest.Companion.track
@@ -17,8 +19,8 @@ import toby.command.commands.music.MusicCommandTest.Companion.trackScheduler
 import java.util.concurrent.ArrayBlockingQueue
 
 internal class NowDigOnThisCommandTest : MusicCommandTest {
-    lateinit var nowDigOnThisCommand: NowDigOnThisCommand
-    
+    private lateinit var nowDigOnThisCommand: NowDigOnThisCommand
+
     @BeforeEach
     fun setUp() {
         setupCommonMusicMocks()
@@ -27,35 +29,43 @@ internal class NowDigOnThisCommandTest : MusicCommandTest {
 
     @AfterEach
     fun tearDown() {
+        clearAllMocks()
     }
 
     @Test
     fun test_nowDigOnThisCommand_withValidArguments() {
         setUpAudioChannelsWithBotAndMemberInSameChannel()
-        val commandContext = CommandContext(CommandTest.event)
-        Mockito.`when`(MusicCommandTest.audioPlayer.isPaused).thenReturn(false)
-        Mockito.`when`(playerManager.isCurrentlyStoppable).thenReturn(false)
-        val linkOptionalMapping = Mockito.mock(OptionMapping::class.java)
-        val volumeOptionalMapping = Mockito.mock(OptionMapping::class.java)
-        Mockito.`when`<OptionMapping>(CommandTest.event.getOption("link")).thenReturn(linkOptionalMapping)
-        Mockito.`when`<OptionMapping>(CommandTest.event.getOption("volume")).thenReturn(volumeOptionalMapping)
-        Mockito.`when`(linkOptionalMapping.asString).thenReturn("www.testlink.com")
-        Mockito.`when`(volumeOptionalMapping.asInt).thenReturn(20)
-        val queue: ArrayBlockingQueue<AudioTrack> = ArrayBlockingQueue<AudioTrack>(2)
-        val track2 = Mockito.mock(
-            AudioTrack::class.java
+        val commandContext = CommandContext(event)
+        every { audioPlayer.isPaused } returns false
+        every { playerManager.isCurrentlyStoppable } returns false
+        every { playerManager.getMusicManager(guild) } returns musicManager
+
+        val linkOptionalMapping = mockk<OptionMapping>()
+        val volumeOptionalMapping = mockk<OptionMapping>()
+        every { event.getOption("link") } returns linkOptionalMapping
+        every { event.getOption("volume") } returns volumeOptionalMapping
+        every { event.getOption("start") } returns mockk(relaxed = true)
+        every { linkOptionalMapping.asString } returns "www.testlink.com"
+        every { volumeOptionalMapping.asInt } returns 20
+        every { playerManager.loadAndPlay(any(), any(), any(), any(), any(), any(), any()) } just Runs
+
+        val queue: ArrayBlockingQueue<AudioTrack> = ArrayBlockingQueue(2)
+        val track2 = mockk<AudioTrack>()
+        every { track2.info } returns AudioTrackInfo(
+            "Another Title",
+            "Another Author",
+            1000L,
+            "identifier",
+            true,
+            "uri"
         )
-        Mockito.`when`(track2.info)
-            .thenReturn(AudioTrackInfo("Another Title", "Another Author", 1000L, "identifier", true, "uri"))
-        Mockito.`when`(track2.duration).thenReturn(1000L)
-        Mockito.`when`(playerManager.getMusicManager(CommandTest.guild))
-            .thenReturn(musicManager)
+        every { track2.duration } returns 1000L
         queue.add(track)
         queue.add(track2)
-        Mockito.`when`(trackScheduler.queue).thenReturn(queue)
-        Mockito.`when`(track.userData).thenReturn(1)
+        every { trackScheduler.queue } returns queue
+        every { track.userData } returns 1
 
-        //Act
+        // Act
         nowDigOnThisCommand.handleMusicCommand(
             commandContext,
             playerManager,
@@ -63,45 +73,52 @@ internal class NowDigOnThisCommandTest : MusicCommandTest {
             0
         )
 
-        Mockito.verify(playerManager, Mockito.times(1)).loadAndPlay(
-            ArgumentMatchers.eq(CommandTest.guild),
-            ArgumentMatchers.eq(CommandTest.event),
-            ArgumentMatchers.eq("www.testlink.com"),
-            ArgumentMatchers.eq(false),
-            ArgumentMatchers.eq(0),
-            ArgumentMatchers.eq(0L),
-            ArgumentMatchers.eq(20)
-        )
+        verify(exactly = 1) {
+            playerManager.loadAndPlay(
+                guild,
+                event,
+                "www.testlink.com",
+                false,
+                0,
+                0L,
+                20
+            )
+        }
     }
 
     @Test
     fun test_nowDigOnThisCommand_withInvalidPermissionsArguments() {
         setUpAudioChannelsWithBotAndMemberInSameChannel()
-        val commandContext = CommandContext(CommandTest.event)
-        Mockito.`when`(MusicCommandTest.audioPlayer.isPaused).thenReturn(false)
-        Mockito.`when`(playerManager.isCurrentlyStoppable).thenReturn(false)
-        val linkOptionalMapping = Mockito.mock(OptionMapping::class.java)
-        val volumeOptionalMapping = Mockito.mock(OptionMapping::class.java)
-        Mockito.`when`<OptionMapping>(CommandTest.event.getOption("link")).thenReturn(linkOptionalMapping)
-        Mockito.`when`<OptionMapping>(CommandTest.event.getOption("volume")).thenReturn(volumeOptionalMapping)
-        Mockito.`when`(linkOptionalMapping.asString).thenReturn("www.testlink.com")
-        Mockito.`when`(volumeOptionalMapping.asInt).thenReturn(20)
-        val queue: ArrayBlockingQueue<AudioTrack> = ArrayBlockingQueue<AudioTrack>(2)
-        val track2 = Mockito.mock(
-            AudioTrack::class.java
+        val commandContext = CommandContext(event)
+        every { audioPlayer.isPaused } returns false
+        every { playerManager.isCurrentlyStoppable } returns false
+
+        val linkOptionalMapping = mockk<OptionMapping>()
+        val volumeOptionalMapping = mockk<OptionMapping>()
+        every { event.getOption("link") } returns linkOptionalMapping
+        every { event.getOption("volume") } returns volumeOptionalMapping
+        every { linkOptionalMapping.asString } returns "www.testlink.com"
+        every { volumeOptionalMapping.asInt } returns 20
+
+        val queue: ArrayBlockingQueue<AudioTrack> = ArrayBlockingQueue(2)
+        val track2 = mockk<AudioTrack>()
+        every { track2.info } returns AudioTrackInfo(
+            "Another Title",
+            "Another Author",
+            1000L,
+            "identifier",
+            true,
+            "uri"
         )
-        Mockito.`when`(track2.info)
-            .thenReturn(AudioTrackInfo("Another Title", "Another Author", 1000L, "identifier", true, "uri"))
-        Mockito.`when`(track2.duration).thenReturn(1000L)
-        Mockito.`when`(playerManager.getMusicManager(CommandTest.guild))
-            .thenReturn(musicManager)
+        every { track2.duration } returns 1000L
+        every { playerManager.getMusicManager(guild) } returns musicManager
         queue.add(track)
         queue.add(track2)
-        Mockito.`when`(trackScheduler.queue).thenReturn(queue)
-        Mockito.`when`(track.userData).thenReturn(1)
-        Mockito.`when`(CommandTest.requestingUserDto.digPermission).thenReturn(false)
+        every { trackScheduler.queue } returns queue
+        every { track.userData } returns 1
+        every { CommandTest.requestingUserDto.digPermission } returns false
 
-        //Act
+        // Act
         nowDigOnThisCommand.handleMusicCommand(
             commandContext,
             playerManager,
@@ -109,16 +126,19 @@ internal class NowDigOnThisCommandTest : MusicCommandTest {
             0
         )
 
-        Mockito.verify(CommandTest.interactionHook, Mockito.times(1))
-            .sendMessage("I'm gonna put some dirt in your eye Effective Name")
-        Mockito.verify(playerManager, Mockito.times(0)).loadAndPlay(
-            ArgumentMatchers.eq(CommandTest.guild),
-            ArgumentMatchers.eq(CommandTest.event),
-            ArgumentMatchers.eq("www.testlink.com"),
-            ArgumentMatchers.eq(false),
-            ArgumentMatchers.eq(0),
-            ArgumentMatchers.eq(0L),
-            ArgumentMatchers.eq(20)
-        )
+        verify(exactly = 1) {
+            event.hook.sendMessage("I'm gonna put some dirt in your eye Effective Name")
+        }
+        verify(exactly = 0) {
+            playerManager.loadAndPlay(
+                guild,
+                event,
+                "www.testlink.com",
+                false,
+                0,
+                0L,
+                20
+            )
+        }
     }
 }
