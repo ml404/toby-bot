e578962b4aab261fbfeb87e1a0e8e74126e1c117
Matthew Layton <15813613+ml404@users.noreply.github.com>
2024-07-19 15:18:11 +0100

    fix meme_command/more logging

--- src/main/kotlin/toby/command/commands/fetch/MemeCommand.kt
+++ src/main/kotlin/toby/command/commands/fetch/MemeCommand.kt
@@ -5,12 +5,12 @@ import com.google.gson.JsonParser
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
-import net.dv8tion.jda.api.interactions.commands.OptionMapping
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
 import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClients
+import org.apache.http.util.EntityUtils
 import toby.command.CommandContext
 import toby.command.ICommand.Companion.invokeDeleteOnMessageResponse
 import toby.dto.web.RedditAPIDto
@@ -18,16 +18,20 @@ import toby.dto.web.RedditAPIDto.TimePeriod
 import toby.jpa.dto.UserDto
 import java.io.IOException
 import java.io.InputStreamReader
-import java.util.*
+import kotlin.random.Random
+import org.slf4j.LoggerFactory
 
 class MemeCommand : IFetchCommand {
     private val SUBREDDIT = "subreddit"
     private val TIME_PERIOD = "timeperiod"
     private val LIMIT = "limit"
+    private val logger = LoggerFactory.getLogger(MemeCommand::class.java)
+
     override fun handle(ctx: CommandContext, requestingUserDto: UserDto, deleteDelay: Int?) {
         try {
             handle(ctx, HttpClients.createDefault(), requestingUserDto, deleteDelay)
         } catch (e: IOException) {
+            logger.error("IOException occurred while handling command", e)
             throw RuntimeException(e)
         }
     }
@@ -36,71 +40,93 @@ class MemeCommand : IFetchCommand {
     fun handle(ctx: CommandContext, httpClient: HttpClient, requestingUserDto: UserDto?, deleteDelay: Int?) {
         val event = ctx.event
         event.deferReply().queue()
-        if (!requestingUserDto!!.memePermission) {
-            sendErrorMessage(event, deleteDelay!!)
+
+        if (requestingUserDto?.memePermission != true) {
+            logger.info("User ${event.member?.effectiveName} does not have meme permission on server ${event.guild?.id}")
+            sendErrorMessage(event, deleteDelay ?: 0)
             return
         }
+
         val result = getRedditApiArgs(event)
-        if (result.subredditArgOptional.isPresent) {
-            val subredditArg = result.subredditArgOptional.get()
-            if (subredditArg == "sneakybackgroundfeet") {
-                event.hook.sendMessageFormat("Don't talk to me.").queue(invokeDeleteOnMessageResponse(deleteDelay!!))
+        val subredditArg = result.subredditArg
+
+        if (subredditArg == "sneakybackgroundfeet") {
+            logger.info("User ${event.member?.effectiveName} requested subreddit 'sneakybackgroundfeet'")
+            event.hook.sendMessageFormat("Don't talk to me.").queue(invokeDeleteOnMessageResponse(deleteDelay ?: 0))
+        } else {
+            val embed = fetchRedditPost(result, event, deleteDelay ?: 0, httpClient)
+            if (embed != null) {
+                logger.info("Successfully fetched meme from subreddit: $subredditArg")
+                event.hook.sendMessageEmbeds(embed).queue()
             } else {
-                val embed = fetchRedditPost(result, event, deleteDelay!!, httpClient)
-                event.hook.sendMessageEmbeds(embed!!).queue()
+                logger.warn("Failed to fetch meme from subreddit: $subredditArg")
+                event.hook.sendMessage("Failed to fetch meme. Please try again later.").queue(invokeDeleteOnMessageResponse(deleteDelay ?: 0))
             }
         }
     }
 
     private fun getRedditApiArgs(event: SlashCommandInteractionEvent): RedditApiArgs {
-        val subredditArgOptional = Optional.ofNullable(event.getOption(SUBREDDIT)).map { obj: OptionMapping -> obj.asString }
-        val timePeriodOptional = Optional.ofNullable(event.getOption(TIME_PERIOD)).map { obj: OptionMapping -> obj.asString }
-        val timePeriod = TimePeriod.parseTimePeriod(timePeriodOptional.orElse("day")).timePeriod
-        val limit = Optional.ofNullable(event.getOption(LIMIT)).map { obj: OptionMapping -> obj.asInt }.orElse(5)
-        return RedditApiArgs(subredditArgOptional, timePeriod, limit)
+        val subredditArg = event.getOption(SUBREDDIT)?.asString
+        val timePeriod = TimePeriod.parseTimePeriod(event.getOption(TIME_PERIOD)?.asString ?: "day").timePeriod
+        val limit = event.getOption(LIMIT)?.asInt ?: 5
+        logger.debug("Reddit API args - Subreddit: $subredditArg, Time Period: $timePeriod, Limit: $limit")
+        return RedditApiArgs(subredditArg, timePeriod, limit)
     }
 
     @Throws(IOException::class)
     private fun fetchRedditPost(result: RedditApiArgs, event: SlashCommandInteractionEvent, deleteDelay: Int, httpClient: HttpClient): MessageEmbed? {
         val gson = Gson()
-        val redditApiUrl = String.format(RedditAPIDto.redditPrefix, result.subredditArgOptional.get(), result.limit, result.timePeriod)
+        val redditApiUrl = String.format(RedditAPIDto.redditPrefix, result.subredditArg, result.limit, result.timePeriod)
         val request = HttpGet(redditApiUrl)
+        logger.info("Fetching Reddit post from URL: $redditApiUrl")
         val response = httpClient.execute(request)
 
-        // Check the response code (200 indicates success)
-        if (response.statusLine.statusCode == 200) {
-            // Parse the JSON response
-            val jsonResponse = JsonParser.parseReader(InputStreamReader(response.entity.content)).getAsJsonObject()
-            val children = jsonResponse.getAsJsonObject("data").getAsJsonArray("children")
-            val random = Random()
+        return response.let { // Using `also` to perform additional operations on the response object
+            if (response.statusLine.statusCode == 200) {
+                val jsonResponse = JsonParser.parseReader(InputStreamReader(response.entity.content)).asJsonObject
+                EntityUtils.consume(response.entity) // Ensure the entity content is fully consumed
 
-            // Extract the data you need from the JSON
-            val meme = children[random.nextInt(children.size())]
-                    .getAsJsonObject()
-                    .getAsJsonObject("data")
-            val redditAPIDto = gson.fromJson(meme.toString(), RedditAPIDto::class.java)
-            if (redditAPIDto.isNsfw!!) {
-                event.hook.sendMessageFormat("I received a NSFW subreddit from %s, or reddit gave me a NSFW meme, either way somebody shoot that guy", event.member).queue(invokeDeleteOnMessageResponse(deleteDelay))
-            } else if (redditAPIDto.video!!) {
-                event.hook.sendMessageFormat("I pulled back a video, whoops. Try again maybe? Or not, up to you.").queue(invokeDeleteOnMessageResponse(deleteDelay))
+                val children = jsonResponse.getAsJsonObject("data").getAsJsonArray("children")
+
+                if (children.size() == 0) {
+                    logger.info("No memes found in subreddit: ${result.subredditArg}")
+                    event.hook.sendMessageFormat("No memes found in the subreddit.").queue(invokeDeleteOnMessageResponse(deleteDelay))
+                    return null
+                }
+
+                val meme = children[Random.nextInt(children.size())].asJsonObject.getAsJsonObject("data")
+                val redditAPIDto = gson.fromJson(meme.toString(), RedditAPIDto::class.java)
+
+                if (redditAPIDto.isNsfw == true) {
+                    logger.warn("NSFW meme detected from subreddit: ${result.subredditArg}")
+                    event.hook.sendMessageFormat("I received a NSFW subreddit from %s, or reddit gave me a NSFW meme, either way somebody shoot that guy", event.member).queue(invokeDeleteOnMessageResponse(deleteDelay))
+                    return null
+                } else if (redditAPIDto.video == true) {
+                    logger.warn("Video meme detected from subreddit: ${result.subredditArg}")
+                    event.hook.sendMessageFormat("I pulled back a video, whoops. Try again maybe? Or not, up to you.").queue(invokeDeleteOnMessageResponse(deleteDelay))
+                    return null
+                }
+
+                val title = meme["title"].asString
+                val url = meme["url"].asString
+                val author = meme["author"].asString
+
+                logger.info("Meme fetched successfully - Title: $title, Author: $author, URL: $url")
+
+                return EmbedBuilder().apply {
+                    setTitle(title, url)
+                    setAuthor(author, null, null)
+                    setImage(url)
+                    addField("subreddit", result.subredditArg ?: "unknown", false)
+                }.build()
+            } else {
+                logger.error("Error response from Reddit API: ${response.statusLine.statusCode}")
+                null
             }
-            val title = meme["title"].asString
-            val url = meme["url"].asString
-            val author = meme["author"].asString
-
-            // Create a JDA EmbedBuilder and return the MessageEmbed
-            val embedBuilder = EmbedBuilder()
-            embedBuilder.setTitle(title, url)
-            embedBuilder.setAuthor(author, null, null)
-            embedBuilder.setImage(url)
-            embedBuilder.addField("subreddit", result.subredditArgOptional.get(), false)
-            return embedBuilder.build()
         }
-        return null // Return null if there was an error
     }
 
-    @JvmRecord
-    private data class RedditApiArgs(val subredditArgOptional: Optional<String>, val timePeriod: String, val limit: Int)
+    private data class RedditApiArgs(val subredditArg: String?, val timePeriod: String, val limit: Int)
 
     override val name: String
         get() = "meme"
@@ -110,7 +136,7 @@ class MemeCommand : IFetchCommand {
         get() {
             val subreddit = OptionData(OptionType.STRING, SUBREDDIT, "Which subreddit to pull the meme from", true)
             val timePeriod = OptionData(OptionType.STRING, TIME_PERIOD, "What time period filter to apply to the subreddit (e.g. day/week/month/all). Default day.", false)
-            Arrays.stream(TimePeriod.entries.toTypedArray()).forEach { tp: TimePeriod -> timePeriod.addChoice(tp.timePeriod, tp.timePeriod) }
+            TimePeriod.entries.forEach { tp -> timePeriod.addChoice(tp.timePeriod, tp.timePeriod) }
             val limit = OptionData(OptionType.INTEGER, LIMIT, "Pick from top X posts of that day. Default 5.", false)
             return listOf(subreddit, timePeriod, limit)
         }
