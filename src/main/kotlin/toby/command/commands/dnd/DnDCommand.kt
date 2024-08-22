ece7330c18f27b39c917833f6f43f10df7504fe4
Matthew Layton <15813613+ml404@users.noreply.github.com>
2024-08-22 14:00:50 +0100

    Extra composition for the DnDCommand

--- src/main/kotlin/toby/command/commands/dnd/DnDCommand.kt
+++ src/main/kotlin/toby/command/commands/dnd/DnDCommand.kt
@@ -1,133 +1,43 @@
 package toby.command.commands.dnd
 
-import kotlinx.coroutines.*
-import mu.KotlinLogging
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
-import net.dv8tion.jda.api.interactions.InteractionHook
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import net.dv8tion.jda.api.interactions.commands.OptionMapping
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
-import net.dv8tion.jda.api.interactions.components.selections.SelectOption
-import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu
-import org.jetbrains.annotations.VisibleForTesting
 import toby.command.CommandContext
-import toby.command.ICommand.Companion.invokeDeleteOnMessageResponse
 import toby.command.commands.fetch.IFetchCommand
-import toby.dto.web.dnd.ApiInfo
-import toby.dto.web.dnd.DnDResponse
-import toby.dto.web.dnd.QueryResult
-import toby.helpers.DnDHelper.doInitialLookup
-import toby.helpers.DnDHelper.queryNonMatchRetry
-import toby.helpers.DnDHelper.toEmbed
 import toby.helpers.HttpHelper
 import toby.jpa.dto.UserDto
 
-class DnDCommand(private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
-                 private val httpHelper: HttpHelper
+class DnDCommand(
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
+    private val httpHelper: HttpHelper
 ) : IDnDCommand, IFetchCommand {
-    private val logger = KotlinLogging.logger {}
 
     override fun handle(ctx: CommandContext, requestingUserDto: UserDto, deleteDelay: Int?) {
         val event = ctx.event
         val typeOptionMapping = event.getOption(TYPE)
-        handleWithHttpObjects(
-            event,
-            getName(typeOptionMapping),
-            typeOptionMapping!!.asString,
-            event.getOption(QUERY)!!.asString,
-            deleteDelay
-        )
-    }
+        val typeName = getName(typeOptionMapping)
+        val typeValue = typeOptionMapping!!.asString
+        val query = event.getOption(QUERY)!!.asString
+        val deleteDelay = deleteDelay ?: 0
 
-    @VisibleForTesting
-    fun handleWithHttpObjects(
-        event: SlashCommandInteractionEvent,
-        typeName: String?,
-        typeValue: String?,
-        query: String,
-        deleteDelay: Int?
-    ) {
         event.deferReply().queue()
         val hook = event.hook
-        logger.info("Accessing DnD API...")
-        // Create a coroutine scope with a supervisor job for structured concurrency
-        val hasRespondedToQuery = createLookupCoroutineAndReply(typeName, typeValue, query, hook, deleteDelay)
 
-        // Only send "No matches found" if no other message was sent
-        if (!hasRespondedToQuery) {
-            logger.info("No matches found for query: $query")
-            hook.sendMessage("Sorry, nothing was returned for $typeName '$query'")
-                .queue(invokeDeleteOnMessageResponse(deleteDelay ?: 0))
-        }
+        // Create and run coroutine scope
+        DnDCommandQueryHandler(dispatcher, httpHelper, hook, deleteDelay).processQuery(typeName, typeValue, query)
     }
 
-    private fun createLookupCoroutineAndReply(
-        typeName: String?,
-        typeValue: String?,
-        query: String,
-        hook: InteractionHook,
-        deleteDelay: Int?
-    ): Boolean {
-        var hasReplied = false
-        CoroutineScope(dispatcher).launch {
-            runCatching {
-                val initialQueryDeferred = async { doInitialLookup(typeName, typeValue, query, httpHelper) }
-                val nonMatchQueryResultDeferred = async { queryNonMatchRetry(typeValue, query, httpHelper) }
-
-                // Handle the initial query result
-                initialQueryDeferred.await()?.let {
-                    if (it.isValidInitialQueryReturn(hook)) {
-                        hasReplied = true
-                        return@launch
-                    }
-                }
-
-                // Handle the non-match query result
-                nonMatchQueryResultDeferred.await()?.let {
-                    if (it.isValidNonMatchQueryReturn(typeName, hook, query)) {
-                        hasReplied = true
-                        return@launch
-                    }
-                }
-
-            }.onFailure {
-                logger.error("An error occurred while handling the DnD query", it)
-                hook.sendMessage("An error occurred while processing your request. Please try again later.")
-                    .queue(invokeDeleteOnMessageResponse(deleteDelay ?: 0))
-                hasReplied = true
-            }
+    private fun getName(typeOptionMapping: OptionMapping?): String {
+        return when (typeOptionMapping!!.asString) {
+            "spells" -> SPELL_NAME
+            "conditions" -> CONDITION_NAME
+            "rule-sections" -> RULE_NAME
+            "features" -> FEATURE_NAME
+            else -> ""
         }
-        return hasReplied
-    }
-
-
-    private fun DnDResponse.isValidInitialQueryReturn(
-        hook: InteractionHook
-    ): Boolean {
-        if (this.isValidReturnObject()) {
-            logger.info("Valid initial query found, sending embed")
-            hook.sendMessageEmbeds(this.toEmbed()).queue()
-            return true
-        }
-        return false
-    }
-
-    private fun QueryResult.isValidNonMatchQueryReturn(
-        typeName: String?,
-        hook: InteractionHook,
-        query: String
-    ): Boolean {
-        logger.info("No initial query result, handling non-match query result")
-        if (this.count > 0) {
-            val builder = StringSelectMenu.create("dnd:$typeName").setPlaceholder("Choose an option")
-            this.results.forEach { info: ApiInfo -> builder.addOptions(SelectOption.of(info.index, info.index)) }
-            val stringSelectMenu = builder.build()
-            hook.sendMessage("Your query '$query' didn't return a value, but these close matches were found, please select one as appropriate")
-                .setActionRow(stringSelectMenu)
-                .queue()
-            return true
-        }
-        return false
     }
 
     override val name: String
@@ -152,15 +62,5 @@ class DnDCommand(private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
         const val CONDITION_NAME = "condition"
         const val RULE_NAME = "rule"
         const val FEATURE_NAME = "feature"
-
-        private fun getName(typeOptionMapping: OptionMapping?): String {
-            return when (typeOptionMapping!!.asString) {
-                "spells" -> SPELL_NAME
-                "conditions" -> CONDITION_NAME
-                "rule-sections" -> RULE_NAME
-                "features" -> FEATURE_NAME
-                else -> ""
-            }
-        }
     }
 }
