3a9991325e6d5d7b7a64a4b3a8a952a91d6bffc5
Matthew Layton <15813613+ml404@users.noreply.github.com>
2024-09-18 13:32:58 +0100

    add test beans

--- src/test/kotlin/configuration/BotConfiguration.kt
+++ src/test/kotlin/configuration/BotConfiguration.kt
@@ -2,14 +2,20 @@ package configuration
 
 import io.mockk.mockk
 import net.dv8tion.jda.api.JDA
+import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Profile
+import toby.BotMain
 
-@Profile("test")
+@TestConfiguration
 open class BotConfiguration {
 
     @Bean
     fun mockJda(): JDA {
-        return mockk() // Replace with a mock JDA instance
+        return mockk()
+    }
+
+    @Bean
+    fun mockBotMain(): BotMain {
+        return mockk()
     }
 }
--- src/test/kotlin/configuration/TestDatabaseConfig.kt
+++ src/test/kotlin/configuration/TestDatabaseConfig.kt
@@ -3,12 +3,11 @@ package configuration
 import io.mockk.mockk
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import toby.jpa.service.IConfigService
-import toby.jpa.service.IMusicFileService
-import toby.jpa.service.IUserService
+import toby.jpa.service.*
 
 @TestConfiguration
 open class TestDatabaseConfig {
+
     @Bean
     open fun userService(): IUserService = mockk(relaxed = true)
 
@@ -17,4 +16,10 @@ open class TestDatabaseConfig {
 
     @Bean
     open fun configService(): IConfigService = mockk(relaxed = true)
+
+    @Bean
+    open fun brotherService(): IBrotherService = mockk(relaxed = true)
+
+    @Bean
+    open fun excuseService(): IExcuseService = mockk(relaxed = true)
 }
