d9e466f2b6d6a1917afae18f52fd3de70dce6066
Matthew Layton <15813613+ml404@users.noreply.github.com>
2024-09-18 15:08:10 +0100

    update youtube audio source manager (#135)
    update youtube audio source manager

--- src/main/kotlin/toby/lavaplayer/PlayerManager.kt
+++ src/main/kotlin/toby/lavaplayer/PlayerManager.kt
@@ -7,10 +7,10 @@ import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers
 import com.sedmelluq.discord.lavaplayer.source.http.HttpAudioSourceManager
 import com.sedmelluq.discord.lavaplayer.source.local.LocalAudioSourceManager
 import com.sedmelluq.discord.lavaplayer.source.twitch.TwitchStreamAudioSourceManager
-import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack
+import dev.lavalink.youtube.YoutubeAudioSourceManager
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import toby.command.ICommand.Companion.invokeDeleteOnMessageResponse
@@ -23,7 +23,9 @@ class PlayerManager(private val audioPlayerManager: AudioPlayerManager) {
     constructor() : this(DefaultAudioPlayerManager())
 
     init {
-        audioPlayerManager.registerSourceManager(YoutubeAudioSourceManager())
+        val youtubeAudioSourceManager = YoutubeAudioSourceManager()
+        youtubeAudioSourceManager.useOauth2(null, false)
+        audioPlayerManager.registerSourceManager(youtubeAudioSourceManager)
         audioPlayerManager.registerSourceManager(TwitchStreamAudioSourceManager())
         audioPlayerManager.registerSourceManager(HttpAudioSourceManager())
         audioPlayerManager.registerSourceManager(LocalAudioSourceManager())
