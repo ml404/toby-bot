35c6d6bc29b2af918e2767e99f4f34da10031e78
Matthew Layton <15813613+ml404@users.noreply.github.com>
2024-09-19 15:04:52 +0100

    more logging

--- src/main/kotlin/toby/helpers/UserDtoHelper.kt
+++ src/main/kotlin/toby/helpers/UserDtoHelper.kt
@@ -1,5 +1,6 @@
 package toby.helpers
 
+import mu.KotlinLogging
 import org.springframework.stereotype.Service
 import toby.jpa.dto.MusicDto
 import toby.jpa.dto.UserDto
@@ -7,11 +8,14 @@ import toby.jpa.service.IUserService
 
 @Service
 class UserDtoHelper(private val userService: IUserService) {
+    private val logger = KotlinLogging.logger {}
+
     fun calculateUserDto(
         guildId: Long,
         discordId: Long,
         isSuperUser: Boolean = false
     ): UserDto {
+        logger.info("Processing lookup for user: $discordId, guild: $guildId")
         return userService.getUserById(discordId, guildId) ?: UserDto().apply {
             this.discordId = discordId
             this.guildId = guildId
