6ae2c9831a50d240ac593f2b3c3e96d4e4f08a9e
Matthew Layton <15813613+ml404@users.noreply.github.com>
2024-09-24 16:00:44 +0100

    edit ephemeral message sent, so not exposing the process to anyone else

--- src/main/kotlin/toby/menu/menus/EditIntroMenu.kt
+++ src/main/kotlin/toby/menu/menus/EditIntroMenu.kt
@@ -32,39 +32,47 @@ class EditIntroMenu(
             logger.info { "Valid musicDto selected." }
 
             event.hook.sendMessage("You've selected ${selectedIntro.fileName}. Please reply with the new volume (0-100).")
-                .setEphemeral(true).queue()
+                .setEphemeral(true)
+                .queue { messageHook ->
+                    // Wait for the user's next message
+                    eventWaiter.waitForMessage(
+                        condition = { msgEvent ->
+                            msgEvent.author.idLong == event.user.idLong && msgEvent.channel.idLong == event.channel.idLong
+                        },
+                        action = { msgEvent ->
+                            logger.info { "Waiting for a response from the user for the new volume" }
 
-            // Wait for the user's next message
-            eventWaiter.waitForMessage(
-                condition = { msgEvent ->
-                    msgEvent.author.idLong == event.user.idLong && msgEvent.channel.idLong == event.channel.idLong
-                },
-                action = { msgEvent ->
-                    logger.info { "Waiting for a response from the user for the new volume" }
+                            val newVolume = msgEvent.message.contentRaw.toIntOrNull()
 
-                    val newVolume = msgEvent.message.contentRaw.toIntOrNull()
+                            if (newVolume != null && newVolume in 0..100) {
+                                selectedIntro.introVolume = newVolume
+                                introHelper.saveIntro(selectedIntro)
 
-                    if (newVolume != null && newVolume in 0..100) {
-                        selectedIntro.introVolume = newVolume
-                        introHelper.saveIntro(selectedIntro)
+                                messageHook.editMessage("Volume updated successfully to $newVolume!")
+                                    .queue { it?.deleteAfter(deleteDelay) }
 
-                        msgEvent.channel.sendMessage("Volume updated successfully to $newVolume!").queue{ it?.deleteAfter(deleteDelay) }
-                        logger.info { "Volume updated successfully to $newVolume!" }
+                                logger.info { "Volume updated successfully to $newVolume!" }
+                                msgEvent.message.deleteAfter(0)
+                            } else {
+                                logger.warn { "Invalid volume was sent" }
 
-                        msgEvent.message.deleteAfter(0)
-                    } else {
-                        logger.warn { "Invalid volume was sent" }
-                        msgEvent.channel.sendMessage("Invalid volume. Please enter a number between 0 and 100.").queue{ it?.deleteAfter(deleteDelay) }
-                        msgEvent.message.deleteAfter(0)
-                    }
-                },
-                timeout = 10000L,  // 10-second timeout
-                timeoutAction = {
-                    event.hook.sendMessage("No response received. Volume update canceled.").setEphemeral(true).queue{ it?.deleteAfter(deleteDelay) }
+                                messageHook.editMessage("Invalid volume. Please enter a number between 0 and 100.")
+                                    .queue { it?.deleteAfter(deleteDelay) }
+
+                                msgEvent.message.deleteAfter(0)
+                            }
+                        },
+                        timeout = 10000L,  // 10-second timeout
+                        timeoutAction = {
+                            messageHook
+                                .editMessage("No response received. Volume update canceled.")
+                                .queue { it?.deleteAfter(deleteDelay) }
+                        }
+                    )
                 }
-            )
         } else {
-            event.hook.sendMessage("Unable to find the selected intro.").setEphemeral(true).queue{ it?.deleteAfter(deleteDelay) }
+            event.hook.sendMessage("Unable to find the selected intro.").setEphemeral(true)
+                .queue { it?.deleteAfter(deleteDelay) }
         }
     }
 
