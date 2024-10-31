be1305c8ff546b198ad2212fef1f176496940241
Matthew Layton <15813613+ml404@users.noreply.github.com>
2024-10-31 14:31:34 +0000

    prevent globbing/splitting

--- bin/trigger_publish_wiki.sh
+++ bin/trigger_publish_wiki.sh
@@ -4,5 +4,5 @@
 curl -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
-  https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/workflows/$WORKFLOW_FILE/dispatches \
+  https://api.github.com/repos/"$REPO_OWNER"/"$REPO_NAME"/actions/workflows/"$WORKFLOW_FILE"/dispatches \
   -d '{"ref": "master"}'
