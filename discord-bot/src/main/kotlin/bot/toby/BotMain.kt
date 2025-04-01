72ee7f54cdf9a1fb3cf9e6bca94bf57bf610f252
Matthew Layton <15813613+ml404@users.noreply.github.com>
2025-04-01 21:27:42 +0100

    better DI for BotMain.kt

--- discord-bot/src/main/kotlin/bot/toby/BotMain.kt
+++ discord-bot/src/main/kotlin/bot/toby/BotMain.kt
@@ -4,12 +4,6 @@ import bot.toby.handler.EventWaiter
 import bot.toby.handler.MessageEventHandler
 import bot.toby.handler.StartUpHandler
 import bot.toby.handler.VoiceEventHandler
-import bot.toby.helpers.IntroHelper
-import bot.toby.helpers.UserDtoHelper
-import core.managers.ButtonManager
-import core.managers.CommandManager
-import core.managers.MenuManager
-import database.service.ConfigService
 import net.dv8tion.jda.api.JDA
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -17,30 +11,12 @@ import org.springframework.stereotype.Service
 @Service
 open class BotMain @Autowired constructor(
     jda: JDA,
-    configService: ConfigService,
-    userDtoHelper: UserDtoHelper,
-    commandManager: CommandManager,
-    buttonManager: ButtonManager,
-    menuManager: MenuManager,
     eventWaiter: EventWaiter,
-    introHelper: IntroHelper
+    startUpHandler: StartUpHandler,
+    voiceEventHandler: VoiceEventHandler,
+    messageEventHandler: MessageEventHandler,
 ) {
     init {
-        jda.addEventListener(
-            StartUpHandler(
-                jda,
-                commandManager
-            )
-        )
-        jda.addEventListener(VoiceEventHandler(jda, configService, userDtoHelper, introHelper))
-        jda.addEventListener(
-            MessageEventHandler(
-                jda,
-                commandManager,
-                buttonManager,
-                menuManager
-            )
-        )
-        jda.addEventListener(eventWaiter)
+        jda.addEventListener(startUpHandler, voiceEventHandler, messageEventHandler, eventWaiter)
     }
 }
