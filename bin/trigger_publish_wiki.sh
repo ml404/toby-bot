a196a57ab3ac4c2b0e5f76444651f7e3aff5c42f
Matthew Layton <15813613+ml404@users.noreply.github.com>
2026-03-13 16:09:14 +0000

    master -> main

--- bin/trigger_publish_wiki.sh
+++ bin/trigger_publish_wiki.sh
@@ -5,4 +5,4 @@ curl -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   https://api.github.com/repos/"$REPO_OWNER"/"$REPO_NAME"/actions/workflows/"$WORKFLOW_FILE"/dispatches \
-  -d '{"ref": "master"}'
+  -d '{"ref": "main"}'
